d4242f8c4ac83599a8135b7030f7cf263e3d9d88
Tomáš Pecka <tomas.pecka@cesnet.cz>
2022-02-16 22:42:49 +0100

    Initialize build system, tests and logs framework
    Add:
     - CI jobs
     - base CMake configuration
     - TSan suppression files
     - Doxyfile
     - clang-format config
     - doctest/trompeloeil integration
    
    Change-Id: I56b7f656549056adfa0a8a6b1104e2026825056f
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/sysrepo-cpp/+/5522

---
+++ src/utils/log.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2016-2018 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+ */
+
+#pragma once
+
+/** @file
+ * @short Include this to be able to produce logs
+ */
+
+#include <spdlog/spdlog.h>
