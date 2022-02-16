373332b5053523f0465dc4009e40bb399f8ec691
Tomáš Pecka <tomas.pecka@cesnet.cz>
2022-02-16 22:42:49 +0100

    Initialize build system, tests and logs framework
    Add:
     - CI jobs
     - base CMake configuration
     - TSan suppression files
     - Doxyfile
     - clang-format config
     - doctest/trompeloeil integration
    
    Change-Id: I56b7f656549056adfa0a8a6b1104e2026825056f
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/sysrepo-cpp/+/5522

---
+++ src/utils/log-init.cpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2016-2019 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+ */
+
+#include <vector>
+#include "utils/log-init.h"
+#include "utils/log.h"
+
+namespace alarms::utils {
+
+/** @short Initialize logging
+
+Creates and registers all required loggers and connect them to the provided sink.
+*/
+void initLogs(std::shared_ptr<spdlog::sinks::sink> sink)
+{
+    for (const auto& name : {"main", "sysrepo"}) {
+        spdlog::register_logger(std::make_shared<spdlog::logger>(name, sink));
+    }
+}
+}
