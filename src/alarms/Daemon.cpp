d613075e0031655b844cdc5015d0c2157df15cf7
Tomáš Pecka <tomas.pecka@cesnet.cz>
2022-02-21 18:26:28 +0100

    daemon: respond to base RPC
    Initialize alarms daemon that can (as of now) accept alarm publications
    and updates from clients over a custom made RPC.
    
    Add following YANG models:
    * ietf-alarms [1] (fetched from [2])
    * sysrepo-ietf-alarms (internal YANG module for RPC communication
      between clients and ietf-alarms daemon)
    * alarms-test (contains alarm identities; testing-only).
    
    [1] https://datatracker.ietf.org/doc/html/rfc8632
    [2] https://github.com/YangModels/yang/blob/91c5f876740db31548808a43a95894bca1c853f9/standard/ietf/RFC/ietf-alarms%402019-09-11.yang
    
    Change-Id: Icf0e8e1a386c98dfde233914bb92fa9dd8cfbdc2

---
+++ src/alarms/Daemon.cpp
@@ -0,0 +1,80 @@
+#include <string>
+#include "Daemon.h"
+#include "utils/libyang.h"
+#include "utils/sysrepo.h"
+
+using namespace std::string_literals;
+
+namespace {
+const auto rpcPrefix = "/sysrepo-ietf-alarms:create-or-update-alarm";
+
+struct AlarmKey {
+    std::string alarmTypeId;
+    std::string alarmTypeQualifier;
+    std::string resource;
+};
+
+AlarmKey getKey(const libyang::DataNode& node)
+{
+    return {
+        alarms::utils::childValue(node, "alarm-type-id"),
+        alarms::utils::childValue(node, "alarm-type-qualifier"),
+        alarms::utils::childValue(node, "resource")};
+}
+
+bool activeAlarmExist(sysrepo::Session& session, const std::string& path)
+{
+    if (auto rootNode = session.getData("/ietf-alarms:alarms")) {
+        for (const auto& node : rootNode->findXPath(path.c_str())) {
+            if (alarms::utils::childValue(node, "is-cleared") == "false") {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+}
+
+
+namespace alarms {
+
+Daemon::Daemon()
+    : m_connection(sysrepo::Connection{})
+    , m_session(m_connection.sessionStart(sysrepo::Datastore::Operational))
+{
+    utils::ensureModuleImplemented(m_session, "ietf-alarms", "2019-09-11");
+    utils::ensureModuleImplemented(m_session, "sysrepo-ietf-alarms", "2022-02-17");
+
+    m_rpcSub = m_session.onRPCAction(rpcPrefix, [&](auto, auto, auto, const libyang::DataNode input, auto, auto, auto) { return submitAlarm(input); });
+}
+
+sysrepo::ErrorCode Daemon::submitAlarm(const libyang::DataNode& input)
+{
+    const auto& alarmKey = getKey(input);
+    const auto severity = std::string(input.findPath("severity").value().asTerm().valueStr());
+    const bool is_cleared = severity == "cleared";
+
+    const auto alarmNodePath = "/ietf-alarms:alarms/alarm-list/alarm[alarm-type-id='"s + alarmKey.alarmTypeId + "'][alarm-type-qualifier='" + alarmKey.alarmTypeQualifier + "'][resource='" + alarmKey.resource + "']";
+
+    // if passing is-cleared=true the alarm either doesn't exist or exists but is inactive (is-cleared=true), do nothing, it's a NOOP
+    if (auto exists = activeAlarmExist(m_session, alarmNodePath); !exists && is_cleared) {
+        return sysrepo::ErrorCode::Ok;
+    }
+
+    m_session.setItem((alarmNodePath + "/is-cleared").c_str(), is_cleared ? "true" : "false");
+
+    if (!is_cleared) {
+        m_session.setItem((alarmNodePath + "/perceived-severity").c_str(), severity.c_str());
+    }
+
+    if (auto node = input.findPath("alarm-text")) {
+        m_session.setItem((alarmNodePath + "/alarm-text").c_str(), std::string(node.value().asTerm().valueStr()).c_str());
+    }
+
+    m_session.applyChanges();
+
+    return sysrepo::ErrorCode::Ok;
+}
+
+}
