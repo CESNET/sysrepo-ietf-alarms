42e79f5b443d93e3d119d9d250f0b7a1b16aa3c3
Tomáš Pecka <tomas.pecka@cesnet.cz>
2022-02-27 13:16:24 +0100

    daemon: support alarm's leaf time-created
    Alarms have a leaf that states when the alarm entry was created. Quoting
    RFC 8632 [1]:
    
    > The timestamp when this alarm entry was created.  This
    > represents the first time the alarm appeared; it can
    > also represent that the alarm reappeared after a purge.
    > Further state changes of the same alarm do not change
    > this leaf; these changes will update the 'last-changed'
    > leaf.
    
    There was small implementation challenge in the testing phase. The
    daemon should publish the current time to sysrepo so we couldn't
    hardcode the times in the tests. There were two possibilities of solving
    that:
    1) Create custom TrivialClock [2] that will be manipulated manually
       from the tests. This would probably require templating Daemon class
       with the TrivialClock used (our custom or the std::system_clock).
    2) Let daemon publish current time in sysrepo and deal with that in the
       tests, probably by allowing some time interval to appear in the
       data.
    
    We chose option number 2. Class AnyTimeBetween representing allowed
    time_point interval was created and custom comparators comparing with
    string YANG values.
    
    [1] https://datatracker.ietf.org/doc/html/rfc8632
    [2] https://en.cppreference.com/w/cpp/named_req/TrivialClock
    
    Change-Id: Iad97981cde4632194cbebaed8ca004b5753acd2f

---
+++ tests/test_time_interval.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2022-2022 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ */
+
+#pragma once
+#include <chrono>
+#include <map>
+#include <string>
+#include <variant>
+
+class AnyTimeBetween {
+    std::chrono::time_point<std::chrono::system_clock> m_start;
+    std::chrono::time_point<std::chrono::system_clock> m_end;
+
+public:
+    AnyTimeBetween(const std::chrono::time_point<std::chrono::system_clock>& least, const std::chrono::time_point<std::chrono::system_clock>& most);
+    bool operator==(const std::string& str) const;
+
+    friend std::ostream& operator<<(std::ostream& os, const AnyTimeBetween& o);
+};
+
+bool operator==(const std::string& str, const AnyTimeBetween& ts);
+
+bool operator==(const std::map<std::string, std::string>& lhs, const std::map<std::string, std::variant<std::string, AnyTimeBetween>>& rhs);
+
+using PropsWithTimeTest = std::map<std::string, std::variant<std::string, AnyTimeBetween>>;
+
+#define SHORTLY_AFTER(point) AnyTimeBetween(point, point + expectedTimeDegreeOfFreedom)
