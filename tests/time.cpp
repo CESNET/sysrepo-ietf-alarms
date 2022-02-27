79a5f083dac378c0c3cb51f5106f1063b05b868a
Tomáš Pecka <tomas.pecka@cesnet.cz>
2022-02-27 13:11:43 +0100

    utils: time formatting function to YANG
    Add a function to print time_point as string in format accepted by
    yang:date-and-time type. Also add a function to convert the string
    representation back to time_point.
    
    Change-Id: I39480e0424cb862e12df329820f9778fef35e04b

---
+++ tests/time.cpp
@@ -0,0 +1,29 @@
+#include "trompeloeil_doctest.h"
+#include <chrono>
+#include <date/date.h>
+#include "utils/time.h"
+
+using namespace std::chrono_literals;
+
+TEST_CASE("Time manipulation")
+{
+    static const auto timestamp = 1645564822; // 2022-02-22 22:20:22
+    static const std::chrono::time_point<std::chrono::system_clock> palindromicTime(std::chrono::seconds(timestamp) + 20ms); // 2022-02-22 22:20:22.02
+    static const auto palindromicTimeYangString = "2022-02-22T21:20:22.020000000-00:00";
+
+    SECTION("date::format doesn't convert to local TZ")
+    {
+        REQUIRE(date::format("%Y-%m-%d %H:%M:%S %z", palindromicTime) == "2022-02-22 21:20:22.020000000 +0000");
+    }
+
+    SECTION("Converts to yang format in UTC TZ")
+    {
+        REQUIRE(alarms::utils::yangTimeFormat(palindromicTime) == palindromicTimeYangString);
+    }
+
+    SECTION("Loads date from yang:date-and-time")
+    {
+        auto loaded = alarms::utils::fromYangTimeFormat(palindromicTimeYangString);
+        REQUIRE(palindromicTime == loaded);
+    }
+}
