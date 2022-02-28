b56955beaacdcd2af6b2a1cd1b335482a00ea601
Tomáš Pecka <tomas.pecka@cesnet.cz>
2022-02-28 15:50:09 +0100

    daemon: purge by severity
    Change-Id: I430c9ab39c5599df5ddaddde5b509466e2e46e69

--- tests/alarm_purge.cpp
+++ tests/alarm_purge.cpp
@@ -14,7 +14,6 @@ namespace {
 const auto rpcPrefix = "/sysrepo-ietf-alarms:create-or-update-alarm";
 const auto purgeRpcPrefix = "/ietf-alarms:alarms/alarm-list/purge-alarms";
 const auto expectedTimeDegreeOfFreedom = 300ms;
-
 }
 
 TEST_CASE("Basic alarm publishing and updating")
@@ -83,7 +82,6 @@ TEST_CASE("Basic alarm publishing and updating")
         SECTION("purge cleared")
         {
             CLIENT_PURGE_RPC(userSess, 1, "cleared", PARAMS());
-
             REQUIRE(dataFromSysrepo(*userSess, "/ietf-alarms:alarms", sysrepo::Datastore::Operational) == std::map<std::string, std::variant<std::string, AnyTimeBetween>>{
                         {"/alarm-list", ""},
                         {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']", ""},
@@ -131,11 +129,125 @@ TEST_CASE("Basic alarm publishing and updating")
         SECTION("purge all")
         {
             CLIENT_PURGE_RPC(userSess, 2, "any", PARAMS());
-
             REQUIRE(dataFromSysrepo(*userSess, "/ietf-alarms:alarms", sysrepo::Datastore::Operational) == std::map<std::string, std::variant<std::string, AnyTimeBetween>>{
                         {"/alarm-list", ""},
                         {"/control", ""},
                     });
+        }
+    }
+
+    SECTION("Purge by clearance status and severity")
+    {
+        CLIENT_ALARM_RPC(time1, cli1Sess, "alarms-test:alarm-1", "", "edfa", "warning", PARAMS());
+        CLIENT_ALARM_RPC(time2, cli1Sess, "alarms-test:alarm-2", "", "edfa", "major", PARAMS());
+
+        REQUIRE(dataFromSysrepo(*userSess, "/ietf-alarms:alarms", sysrepo::Datastore::Operational) == std::map<std::string, std::variant<std::string, AnyTimeBetween>>{
+                    {"/alarm-list", ""},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='']", ""},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='']/alarm-type-id", "alarms-test:alarm-1"},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='']/alarm-type-qualifier", ""},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='']/resource", "edfa"},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='']/is-cleared", "false"},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='']/perceived-severity", "warning"},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='']/time-created", EXPECT_TIME_INTERVAL(time1)},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='']/last-raised", EXPECT_TIME_INTERVAL(time1)},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='']/last-changed", EXPECT_TIME_INTERVAL(time1)},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']", ""},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/alarm-type-id", "alarms-test:alarm-2"},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/alarm-type-qualifier", ""},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/resource", "edfa"},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/is-cleared", "false"},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/perceived-severity", "major"},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/time-created", EXPECT_TIME_INTERVAL(time2)},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/last-raised", EXPECT_TIME_INTERVAL(time2)},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/last-changed", EXPECT_TIME_INTERVAL(time2)},
+                    {"/control", ""},
+                });
+
+        CLIENT_ALARM_RPC(time3, cli1Sess, "alarms-test:alarm-1", "", "edfa", "cleared", PARAMS());
+
+        REQUIRE(dataFromSysrepo(*userSess, "/ietf-alarms:alarms", sysrepo::Datastore::Operational) == std::map<std::string, std::variant<std::string, AnyTimeBetween>>{
+                    {"/alarm-list", ""},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='']", ""},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='']/alarm-type-id", "alarms-test:alarm-1"},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='']/alarm-type-qualifier", ""},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='']/resource", "edfa"},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='']/is-cleared", "true"},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='']/perceived-severity", "warning"},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='']/time-created", EXPECT_TIME_INTERVAL(time1)},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='']/last-raised", EXPECT_TIME_INTERVAL(time1)},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='']/last-changed", EXPECT_TIME_INTERVAL(time1)},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']", ""},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/alarm-type-id", "alarms-test:alarm-2"},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/alarm-type-qualifier", ""},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/resource", "edfa"},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/is-cleared", "false"},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/perceived-severity", "major"},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/time-created", EXPECT_TIME_INTERVAL(time2)},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/last-raised", EXPECT_TIME_INTERVAL(time2)},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/last-changed", EXPECT_TIME_INTERVAL(time2)},
+                    {"/alarm-list/alarm[resource='edfa'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']", ""},
+                    {"/control", ""},
+                });
+
+        SECTION("below")
+        {
+            SECTION("below warning")
+            {
+                CLIENT_PURGE_RPC(userSess, 0, "any", PARAMS(P("severity/below", "warning")));
+            }
+            SECTION("below indeterminate")
+            {
+                CLIENT_PURGE_RPC(userSess, 0, "any", PARAMS(P("severity/below", "indeterminate")));
+            }
+            SECTION("below major")
+            {
+                CLIENT_PURGE_RPC(userSess, 1, "any", PARAMS(P("severity/below", "major")));
+            }
+            SECTION("below critical")
+            {
+                CLIENT_PURGE_RPC(userSess, 2, "any", PARAMS(P("severity/below", "critical")));
+            }
+        }
+
+        SECTION("is")
+        {
+            SECTION("is warning")
+            {
+                CLIENT_PURGE_RPC(userSess, 1, "any", PARAMS(P("severity/is", "warning")));
+            }
+            SECTION("is indeterminate")
+            {
+                CLIENT_PURGE_RPC(userSess, 0, "any", PARAMS(P("severity/is", "indeterminate")));
+            }
+            SECTION("is major")
+            {
+                CLIENT_PURGE_RPC(userSess, 1, "any", PARAMS(P("severity/is", "major")));
+            }
+            SECTION("is critical")
+            {
+                CLIENT_PURGE_RPC(userSess, 0, "any", PARAMS(P("severity/is", "critical")));
+            }
+        }
+
+        SECTION("above")
+        {
+            SECTION("above warning")
+            {
+                CLIENT_PURGE_RPC(userSess, 1, "any", PARAMS(P("severity/above", "warning")));
+            }
+            SECTION("above indeterminate")
+            {
+                CLIENT_PURGE_RPC(userSess, 2, "any", PARAMS(P("severity/above", "indeterminate")));
+            }
+            SECTION("above major")
+            {
+                CLIENT_PURGE_RPC(userSess, 0, "any", PARAMS(P("severity/above", "major")));
+            }
+            SECTION("above critical")
+            {
+                CLIENT_PURGE_RPC(userSess, 0, "any", PARAMS(P("severity/above", "critical")));
+            }
         }
     }
 }
