d0bf51a2895b38c9db1843e37c903d0f0e7ded73
Tomáš Pecka <tomas.pecka@cesnet.cz>
2022-03-01 14:27:49 +0100

    daemon: implement purging alarms by age
    Change-Id: Ife178439d4e4450e402257515ac794dd04adfdd5

--- src/alarms/PurgeFilter.cpp
+++ src/alarms/PurgeFilter.cpp
@@ -6,10 +6,20 @@
  */
 
 #include <algorithm>
+#include <chrono>
 #include <libyang-cpp/Value.hpp>
 #include "PurgeFilter.h"
 #include "utils/libyang.h"
+#include "utils/time.h"
 
+namespace {
+
+template <class T>
+T getValue(const libyang::DataNode& node)
+{
+    return std::get<T>(node.asTerm().value());
+}
+}
 namespace alarms {
 
 PurgeFilter::PurgeFilter(const libyang::DataNode& filterInput)
@@ -49,6 +59,30 @@ PurgeFilter::PurgeFilter(const libyang::DataNode& filterInput)
             auto severityNode = alarmNode.findPath("perceived-severity");
             auto enumVal = std::get<libyang::Enum>(severityNode->asTerm().value()).value;
             return severityCheck(enumVal);
+        });
+    }
+
+    if (auto olderThanContainer = filterInput.findPath("older-than")) {
+        auto threshold = std::chrono::system_clock::now();
+
+        if (auto choice = olderThanContainer->findPath("seconds")) {
+            threshold = threshold - std::chrono::seconds(getValue<uint16_t>(*choice));
+        } else if (auto choice = olderThanContainer->findPath("minutes")) {
+            threshold = threshold - std::chrono::minutes(getValue<uint16_t>(*choice));
+        } else if (auto choice = olderThanContainer->findPath("hours")) {
+            threshold = threshold - std::chrono::hours(getValue<uint16_t>(*choice));
+        } else if (auto choice = olderThanContainer->findPath("days")) {
+            threshold = threshold - std::chrono::days(getValue<uint16_t>(*choice));
+        } else if (auto choice = olderThanContainer->findPath("weeks")) {
+            threshold = threshold - 7 * std::chrono::days(getValue<uint16_t>(*choice));
+        } else {
+            throw std::logic_error("Invalid choice value");
+        }
+
+        m_filters.emplace_back([threshold](const libyang::DataNode& alarmNode) {
+            auto lastChangedNode = alarmNode.findPath("last-changed");
+            auto alarmTime = utils::fromYangTimeFormat(std::string(lastChangedNode->asTerm().valueStr()));
+            return alarmTime < threshold;
         });
     }
 }
