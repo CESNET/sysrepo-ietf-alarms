2c1f4c92d6d2b3ebd0f24215c0f2a49280688643
Tomáš Pecka <tomas.pecka@cesnet.cz>
2022-03-13 19:30:58 +0100

    daemon: send alarm status change notifications
    The IETF model ietf-alarms [1] supports sending notifications about the
    alarm state changes.
    This commit sends YANG notification whenever alarm state changes (which
    is the default behaviour unless configured otherwise -- see next
    commit).
    
    [1] https://datatracker.ietf.org/doc/html/rfc8632
    
    Change-Id: Ieaf70c0f02fd48ecacb2c7c800154036c63c5736

---
+++ tests/events.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2016-2018 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+ */
+
+#pragma once
+
+#include <chrono>
+#include <map>
+#include <mutex>
+#include <sysrepo-cpp/Session.hpp>
+#include "test_log_setup.h"
+
+/** @short YANG notifications */
+class EventWatcher {
+public:
+    struct Event {
+        std::string xPath;
+        std::map<std::string, std::string> data;
+        std::chrono::time_point<std::chrono::steady_clock> received;
+    };
+
+    explicit EventWatcher(std::function<void(Event)> callback);
+    ~EventWatcher();
+    void operator()(sysrepo::Session session, uint32_t subscriptionId, const sysrepo::NotificationType type, const std::optional<libyang::DataNode> notificationTree, const sysrepo::NotificationTimeStamp timestamp);
+
+    Event peek(std::vector<Event>::size_type index) const;
+    std::vector<Event>::size_type count() const;
+
+private:
+    std::function<void(Event)> notifRecvCb;
+    mutable std::shared_ptr<std::mutex> mutex = std::make_shared<std::mutex>();
+    std::shared_ptr<std::vector<Event>> events = std::make_shared<std::vector<Event>>();
+};
