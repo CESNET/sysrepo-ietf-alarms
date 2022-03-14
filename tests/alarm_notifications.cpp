fa5c6cb6cf870901358392571625965fd4f4ce28
Tomáš Pecka <tomas.pecka@cesnet.cz>
2022-03-14 09:26:43 +0100

    daemon: implement status change notification settings
    There are leaves notify-status-changes and notify-severity-level under
    alarms/control container in the ietf-alarms module. This leaves control
    when to send notification about status changes.
    
    In this commit, we implement sending notifications based on the values
    found in the control container.
    
    Change-Id: I77ad7430b3a1a9624e78622594bc55007682b522

--- tests/alarm_notifications.cpp
+++ tests/alarm_notifications.cpp
@@ -82,12 +82,56 @@ TEST_CASE("Receiving alarm notifications")
         },
         alarmStatusNotification);
 
-    CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli1Sess, "alarms-test:alarm-1", "", "edfa", "warning", "Hey, I'm overheating.");
-    CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli2Sess, "alarms-test:alarm-1", "qual", "idk", "minor", ":-)");
-    CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli1Sess, "alarms-test:alarm-1", "", "edfa", "major", "Hey, I'm overheating.");
-    CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli2Sess, "alarms-test:alarm-1", "", "edfa", "major", "Text change.");
-    CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli1Sess, "alarms-test:alarm-1", "", "edfa", "cleared", "Temperature normal.");
-    CLIENT_ALARM_RPC(cli1Sess, "alarms-test:alarm-1", "", "edfa", "cleared", "Temperature normal.");
+    SECTION("notifications: all-status-changes")
+    {
+        CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli1Sess, "alarms-test:alarm-1", "", "edfa", "warning", "Hey, I'm overheating.");
+        CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli2Sess, "alarms-test:alarm-1", "qual", "idk", "minor", ":-)");
+        CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli1Sess, "alarms-test:alarm-1", "", "edfa", "major", "Hey, I'm overheating.");
+        CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli2Sess, "alarms-test:alarm-1", "", "edfa", "major", "Text change.");
+        CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli1Sess, "alarms-test:alarm-1", "", "edfa", "cleared", "Temperature normal.");
+        CLIENT_ALARM_RPC(cli1Sess, "alarms-test:alarm-1", "", "edfa", "cleared", "Temperature normal.");
+    }
+
+    SECTION("notifications: raise-and-clear")
+    {
+        userSess->setItem("/ietf-alarms:alarms/control/notify-status-changes", "raise-and-clear");
+        userSess->applyChanges();
+
+        CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli1Sess, "alarms-test:alarm-1", "", "edfa", "warning", "Hey, I'm overheating.");
+        CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli2Sess, "alarms-test:alarm-1", "qual", "idk", "minor", ":-)");
+        CLIENT_ALARM_RPC(cli1Sess, "alarms-test:alarm-1", "", "edfa", "major", "Hey, I'm overheating.");
+        CLIENT_ALARM_RPC(cli2Sess, "alarms-test:alarm-1", "", "edfa", "major", "Text change.");
+        CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli1Sess, "alarms-test:alarm-1", "", "edfa", "cleared", "Temperature normal.");
+        CLIENT_ALARM_RPC(cli1Sess, "alarms-test:alarm-1", "", "edfa", "cleared", "Temperature normal.");
+        CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli1Sess, "alarms-test:alarm-1", "", "edfa", "warning", "Hey, I'm overheating.");
+    }
+
+    SECTION("notifications: severity-level")
+    {
+        userSess->setItem("/ietf-alarms:alarms/control/notify-status-changes", "severity-level");
+        userSess->setItem("/ietf-alarms:alarms/control/notify-severity-level", "major");
+        userSess->applyChanges();
+
+        /* The following test case is from YANG model ietf-alarms@2019-09-11,
+         * found under description to the leaf ietf-alarms:alarms/control/notify-status-changes
+         *
+         * [(Time, severity, clear)]
+         * [(T1, major, -), (T2, minor, -), (T3, warning, -), (T4, minor, -),
+         *  (T5, major, -), (T6, critical, -), (T7, major.  -), (T8, major, clear)]
+
+         * In that case, notifications will be sent at times
+         * T1, T2, T5, T6, T7, and T8.
+         */
+
+        CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli1Sess, "alarms-test:alarm-1", "", "edfa", "major", "T1");
+        CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli1Sess, "alarms-test:alarm-1", "", "edfa", "minor", "T2");
+        CLIENT_ALARM_RPC(cli1Sess, "alarms-test:alarm-1", "", "edfa", "warning", "T3");
+        CLIENT_ALARM_RPC(cli1Sess, "alarms-test:alarm-1", "", "edfa", "minor", "T4");
+        CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli1Sess, "alarms-test:alarm-1", "", "edfa", "major", "T5");
+        CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli1Sess, "alarms-test:alarm-1", "", "edfa", "critical", "T6");
+        CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli1Sess, "alarms-test:alarm-1", "", "edfa", "major", "T7");
+        CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli1Sess, "alarms-test:alarm-1", "", "edfa", "cleared", "T8");
+    }
 
     waitForCompletionAndBitMore(seq1);
 
