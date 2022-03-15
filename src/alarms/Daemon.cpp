CESNET/sysrepo-ietf-alarms

commit fa14c38823eda05adebd5b9ffe952c68cb48197c

daemon: respond to base RPC
Initialize alarms daemon that can (as of now) accept alarm publications
and updates from clients over a custom made RPC.

Add following YANG models:
* ietf-alarms [1] (fetched from [2])
* sysrepo-ietf-alarms (internal YANG module for RPC communication
  between clients and ietf-alarms daemon)
* alarms-test (contains alarm identities; testing-only).

[1] https://datatracker.ietf.org/doc/html/rfc8632
[2] https://github.com/YangModels/yang/blob/91c5f876740db31548808a43a95894bca1c853f9/standard/ietf/RFC/ietf-alarms%402019-09-11.yang

Change-Id: Icf0e8e1a386c98dfde233914bb92fa9dd8cfbdc2

src/alarms/Daemon.cpp
@@ -1,0 +1,105 @@
+#include <string>
+#include "Daemon.h"
+#include "utils/libyang.h"
+#include "utils/log.h"
+#include "utils/sysrepo.h"
+
+using namespace std::string_literals;
+
+namespace {
+const auto rpcPrefix = "/sysrepo-ietf-alarms:create-or-update-alarm";
+
+/** @brief Escapes key with the other type of quotes than found in the string.
+ *
+ *  @throws std::invalid_argument if both single and double quotes used in the input
+ * */
+std::string escapeListKey(const std::string& str)
+{
+    auto singleQuotes = str.find('\'') != std::string::npos;
+    auto doubleQuotes = str.find('\"') != std::string::npos;
+
+    if (singleQuotes && doubleQuotes) {
+        throw std::invalid_argument("Encountered mixed single and double quotes in XPath; can't properly escape.");
+    } else if (singleQuotes) {
+        return '\"' + str + '\"';
+    } else {
+        return '\'' + str + '\'';
+    }
+}
+
+struct AlarmKey {
+    std::string alarmTypeId;
+    std::string alarmTypeQualifier;
+    std::string resource;
+};
+
+AlarmKey getKey(const libyang::DataNode& node)
+{
+    return {
+        alarms::utils::childValue(node, "alarm-type-id"),
+        alarms::utils::childValue(node, "alarm-type-qualifier"),
+        alarms::utils::childValue(node, "resource")};
+}
+
+/** @brief Checks whether node specified by xpath exists in the tree */
+bool activeAlarmExist(sysrepo::Session& session, const std::string& path)
+{
+    if (auto data = session.getData(path.c_str())) {
+        return static_cast<bool>(data->findPath(path.c_str()));
+    }
+    return false;
+}
+}
+
+
+namespace alarms {
+
+Daemon::Daemon()
+    : m_connection(sysrepo::Connection{})
+    , m_session(m_connection.sessionStart(sysrepo::Datastore::Operational))
+    , m_log(spdlog::get("main"))
+{
+    utils::ensureModuleImplemented(m_session, "ietf-alarms", "2019-09-11");
+    utils::ensureModuleImplemented(m_session, "sysrepo-ietf-alarms", "2022-02-17");
+
+    m_rpcSub = m_session.onRPCAction(rpcPrefix, [&](sysrepo::Session session, auto, auto, const libyang::DataNode input, auto, auto, auto) { return submitAlarm(session, input); });
+}
+
+sysrepo::ErrorCode Daemon::submitAlarm(sysrepo::Session rpcSession, const libyang::DataNode& input)
+{
+    const auto& alarmKey = getKey(input);
+    const auto severity = std::string(input.findPath("severity").value().asTerm().valueStr());
+    const bool is_cleared = severity == "cleared";
+
+    try {
+        const auto alarmNodePath = "/ietf-alarms:alarms/alarm-list/alarm[alarm-type-id='"s + alarmKey.alarmTypeId + "'][alarm-type-qualifier='" + alarmKey.alarmTypeQualifier + "'][resource=" + escapeListKey(alarmKey.resource) + "]";
+        m_log->trace("Alarm node: {}", alarmNodePath);
+
+        // if passing is-cleared=true the alarm either doesn't exist or exists but is inactive (is-cleared=true), do nothing, it's a NOOP
+        if (auto exists = activeAlarmExist(m_session, alarmNodePath); !exists && is_cleared) {
+            return sysrepo::ErrorCode::Ok;
+        }
+
+        auto edit = m_session.getContext().newPath(alarmNodePath.c_str(), nullptr, libyang::CreationOptions::Update);
+
+        edit.newPath((alarmNodePath + "/is-cleared").c_str(), is_cleared ? "true" : "false", libyang::CreationOptions::Update);
+
+        if (!is_cleared) {
+            edit.newPath((alarmNodePath + "/perceived-severity").c_str(), severity.c_str(), libyang::CreationOptions::Update);
+        }
+
+        edit.newPath((alarmNodePath + "/alarm-text").c_str(), std::string(input.findPath("alarm-text").value().asTerm().valueStr()).c_str(), libyang::CreationOptions::Update);
+
+        m_log->trace("Update: {}", std::string(*edit.printStr(libyang::DataFormat::JSON, libyang::PrintFlags::Shrink)));
+        m_session.editBatch(edit, sysrepo::DefaultOperation::Merge);
+        m_session.applyChanges();
+
+        return sysrepo::ErrorCode::Ok;
+    } catch (const std::invalid_argument& e) {
+        m_log->debug("submitAlarm exception: {}", e.what());
+        rpcSession.setErrorMessage(e.what());
+        return sysrepo::ErrorCode::InvalidArgument;
+    }
+}
+
+}

CMakeLists.txt
@@ -31,19 +31,32 @@ include(GNUInstallDirs)
 
 include_directories(${CMAKE_CURRENT_SOURCE_DIR}/src/)
 
-set(YANG_SRCS "")
+set(YANG_SRCS
+    yang/ietf-alarms.yang
+    yang/sysrepo-ietf-alarms.yang
+    )
 
 # Targets
 # - utils
 add_library(alarms-utils STATIC
+    src/utils/libyang.cpp
+    src/utils/libyang.h
     src/utils/log.h
     src/utils/log-fwd.h
     src/utils/log-init.cpp
     src/utils/log-init.h
     src/utils/string.cpp
     src/utils/string.h
+    src/utils/sysrepo.cpp
+    src/utils/sysrepo.h
     )
-target_link_libraries(alarms-utils PUBLIC spdlog::spdlog PRIVATE fmt::fmt)
+target_link_libraries(alarms-utils PUBLIC spdlog::spdlog PRIVATE fmt::fmt PkgConfig::LIBYANG PkgConfig::SYSREPO)
+
+add_library(alarms STATIC
+    src/alarms/Daemon.cpp
+    src/alarms/Daemon.h
+    )
+target_link_libraries(alarms PUBLIC alarms-utils PRIVATE PkgConfig::LIBYANG)
 
 # Testing
 include(CTest)
@@ -62,6 +75,7 @@ if(BUILD_TESTING)
 
     add_library(Testing STATIC
         tests/test_log_setup.h
+        tests/test_alarm_helpers.h
         tests/test_sysrepo_helpers.cpp
         tests/test_sysrepo_helpers.h
         )
@@ -70,6 +84,10 @@ if(BUILD_TESTING)
 
     find_program(SYSREPOCTL sysrepoctl)
     find_program(SYSREPOCFG sysrepocfg)
+
+    set(YANG_TEST_SRCS
+        tests/yang/alarms-test.yang
+        )
 
     function(ietfalarms_test name)
         add_executable(test-${name} ${CMAKE_SOURCE_DIR}/tests/${name}.cpp)
@@ -110,12 +128,20 @@ if(BUILD_TESTING)
         endif()
     endfunction()
 
+    set(fixture-alarms_testing
+        YANG ${CMAKE_CURRENT_SOURCE_DIR}/yang/ietf-alarms.yang
+        YANG ${CMAKE_CURRENT_SOURCE_DIR}/yang/sysrepo-ietf-alarms.yang
+        YANG ${CMAKE_CURRENT_SOURCE_DIR}/tests/yang/alarms-test.yang
+        )
+    ietfalarms_test(alarm_publish fixture-alarms_testing)
+    target_link_libraries(test-alarm_publish alarms)
+
     find_program(YANGLINT_PATH yanglint)
     if (NOT YANGLINT_PATH)
         message(FATAL_ERROR "Cannot find libyang's yanglint")
     endif()
-    foreach(yang_file ${YANG_SRCS})
-        add_test(NAME yang-tree:${yang_file} COMMAND ${YANGLINT_PATH} -f tree ${CMAKE_CURRENT_SOURCE_DIR}/${yang_file})
+    foreach(yang_file ${YANG_SRCS} ${YANG_TEST_SRCS})
+        add_test(NAME yang-tree:${yang_file} WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/yang COMMAND ${YANGLINT_PATH} -f tree ${CMAKE_CURRENT_SOURCE_DIR}/${yang_file})
     endforeach()
 endif()
 

src/alarms/Daemon.h
@@ -1,0 +1,21 @@
+#pragma once
+#include <optional>
+#include <sysrepo-cpp/Connection.hpp>
+#include "utils/log-fwd.h"
+
+namespace alarms {
+
+class Daemon {
+public:
+    Daemon();
+
+private:
+    sysrepo::Connection m_connection;
+    sysrepo::Session m_session;
+    std::optional<sysrepo::Subscription> m_rpcSub;
+    alarms::Log m_log;
+
+    sysrepo::ErrorCode submitAlarm(sysrepo::Session rpcSession, const libyang::DataNode& input);
+};
+
+}

src/utils/libyang.cpp
@@ -1,0 +1,26 @@
+/*
+ * Copyright (C) 2022 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+ */
+
+#include <libyang-cpp/DataNode.hpp>
+#include "utils/libyang.h"
+
+namespace alarms::utils {
+/** @brief Extract text value of a leaf which is a child of the given parent */
+std::string childValue(const libyang::DataNode& node, const std::string& leafName)
+{
+    auto leaf = node.findPath(leafName.c_str());
+
+    if (!leaf) {
+        throw std::runtime_error("Selected child does not exist");
+    }
+    if (!leaf->isTerm()) {
+        throw std::runtime_error("Selected child is not a leaf");
+    }
+
+    return std::string(leaf->asTerm().valueStr());
+}
+}

src/utils/libyang.h
@@ -1,0 +1,17 @@
+/*
+ * Copyright (C) 2022 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+ */
+
+#pragma once
+#include <string>
+
+namespace libyang {
+class DataNode;
+}
+
+namespace alarms::utils {
+std::string childValue(const libyang::DataNode& node, const std::string& name);
+}

src/utils/sysrepo.cpp
@@ -1,0 +1,60 @@
+/*
+ * Copyright (C) 2016-2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ */
+
+#include "sysrepo.h"
+#include "utils/log.h"
+
+extern "C" {
+#include <sysrepo.h>
+}
+
+extern "C" {
+/** @short Propagate sysrepo events to spdlog */
+static void spdlog_sr_log_cb(sr_log_level_t level, const char* message)
+{
+    // Thread safety note: this is, as far as I know, thread safe:
+    // - the static initialization itself is OK
+    // - all loggers which we instantiate are thread-safe
+    // - std::shared_ptr::operator-> is const, and all const members of that class are documented to be thread-safe
+    static auto log = spdlog::get("sysrepo");
+    assert(log);
+    switch (level) {
+    case SR_LL_NONE:
+    case SR_LL_ERR:
+        log->error(message);
+        break;
+    case SR_LL_WRN:
+        log->warn(message);
+        break;
+    case SR_LL_INF:
+        log->info(message);
+        break;
+    case SR_LL_DBG:
+        log->debug(message);
+        break;
+    }
+}
+}
+
+namespace alarms::utils {
+
+/** @short Setup sysrepo log forwarding
+You must call cla::utils::initLogs prior to this function.
+*/
+void initLogsSysrepo()
+{
+    sr_log_set_cb(spdlog_sr_log_cb);
+}
+
+/** @brief Checks whether a module is implemented in Sysrepo and throws if not. */
+void ensureModuleImplemented(const sysrepo::Session& session, const std::string& module, const std::string& revision)
+{
+    if (auto mod = session.getContext().getModule(module.c_str(), revision.c_str()); !mod || !mod->implemented()) {
+        throw std::runtime_error(module + "@" + revision + " is not implemented in sysrepo.");
+    }
+}
+
+}

src/utils/sysrepo.h
@@ -1,0 +1,17 @@
+/*
+ * Copyright (C) 2020 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ */
+
+#pragma once
+
+#include <string>
+#include <sysrepo-cpp/Session.hpp>
+
+namespace alarms::utils {
+
+void initLogsSysrepo();
+void ensureModuleImplemented(const sysrepo::Session& session, const std::string& module, const std::string& revision);
+
+}

commit 486d2ada59ec538bb9ec182b553f6b0e2314e2cb

daemon: shorten main RPC try-block
The only thing that throws is the list key escaping when building xPath
of the alarm node. I don't think that the (almost) whole method must be
enclosed in the try block.

Fixes: fa14c38823eda05adebd5b9ffe952c68cb48197c

Change-Id: I7f96145e9831b8c84882be3ba0df846e8375bbe2

src/alarms/Daemon.cpp
@@ -140,51 +140,54 @@ sysrepo::ErrorCode Daemon::submitAlarm(sysrepo::Session rpcSession, const libyan
     const bool is_cleared = severity == "cleared";
     const auto now = std::chrono::system_clock::now();
 
+    std::string alarmNodePath;
+
     try {
-        const auto alarmNodePath = "/ietf-alarms:alarms/alarm-list/alarm[alarm-type-id='"s + alarmKey.alarmTypeId + "'][alarm-type-qualifier='" + alarmKey.alarmTypeQualifier + "'][resource=" + escapeListKey(alarmKey.resource) + "]";
+        alarmNodePath = "/ietf-alarms:alarms/alarm-list/alarm[alarm-type-id='"s + alarmKey.alarmTypeId + "'][alarm-type-qualifier='" + alarmKey.alarmTypeQualifier + "'][resource=" + escapeListKey(alarmKey.resource) + "]";
         m_log->trace("Alarm node: {}", alarmNodePath);
-        const auto existingAlarmNode = activeAlarmExist(m_session, alarmNodePath);
-
-        auto edit = m_session.getContext().newPath(alarmNodePath.c_str(), nullptr, libyang::CreationOptions::Update);
-
-        if (is_cleared && (!existingAlarmNode || (existingAlarmNode && utils::childValue(*existingAlarmNode, "is-cleared") == "true"))) {
-            // if passing is-cleared=true the alarm either doesn't exist or exists but is inactive (is-cleared=true), do nothing, it's a NOOP
-            return sysrepo::ErrorCode::Ok;
-        } else if (!existingAlarmNode) {
-            edit.newPath((alarmNodePath + "/time-created").c_str(), utils::yangTimeFormat(now).c_str());
-        }
-
-        edit.newPath((alarmNodePath + "/is-cleared").c_str(), is_cleared ? "true" : "false", libyang::CreationOptions::Update);
-        if (!is_cleared && (!existingAlarmNode || (existingAlarmNode && utils::childValue(*existingAlarmNode, "is-cleared") == "true"))) {
-            edit.newPath((alarmNodePath + "/last-raised").c_str(), utils::yangTimeFormat(now).c_str());
-        }
-
-        if (!is_cleared) {
-            edit.newPath((alarmNodePath + "/perceived-severity").c_str(), severity.c_str(), libyang::CreationOptions::Update);
-        }
-
-        edit.newPath((alarmNodePath + "/alarm-text").c_str(), std::string(input.findPath("alarm-text").value().asTerm().valueStr()).c_str(), libyang::CreationOptions::Update);
-
-        const auto& editAlarmNode = edit.findPath(alarmNodePath.c_str());
-        if (valueChanged(existingAlarmNode, *editAlarmNode, "alarm-text") || valueChanged(existingAlarmNode, *editAlarmNode, "is-cleared") || valueChanged(existingAlarmNode, *editAlarmNode, "perceived-severity")) {
-            edit.newPath((alarmNodePath + "/last-changed").c_str(), utils::yangTimeFormat(now).c_str());
-        }
-
-        m_log->trace("Update: {}", std::string(*edit.printStr(libyang::DataFormat::JSON, libyang::PrintFlags::Shrink)));
-        m_session.editBatch(edit, sysrepo::DefaultOperation::Merge);
-        m_session.applyChanges();
-
-        if (shouldNotifyStatusChange(m_session, existingAlarmNode, *editAlarmNode)) {
-            auto notification = createStatusChangeNotification(alarmNodePath);
-            m_session.sendNotification(notification, sysrepo::Wait::No);
-        }
-
-        return sysrepo::ErrorCode::Ok;
     } catch (const std::invalid_argument& e) {
         m_log->debug("submitAlarm exception: {}", e.what());
         rpcSession.setErrorMessage(e.what());
         return sysrepo::ErrorCode::InvalidArgument;
     }
+
+    const auto existingAlarmNode = activeAlarmExist(m_session, alarmNodePath);
+
+    auto edit = m_session.getContext().newPath(alarmNodePath.c_str(), nullptr, libyang::CreationOptions::Update);
+
+    if (is_cleared && (!existingAlarmNode || (existingAlarmNode && utils::childValue(*existingAlarmNode, "is-cleared") == "true"))) {
+        // if passing is-cleared=true the alarm either doesn't exist or exists but is inactive (is-cleared=true), do nothing, it's a NOOP
+        return sysrepo::ErrorCode::Ok;
+    } else if (!existingAlarmNode) {
+        edit.newPath((alarmNodePath + "/time-created").c_str(), utils::yangTimeFormat(now).c_str());
+    }
+
+    edit.newPath((alarmNodePath + "/is-cleared").c_str(), is_cleared ? "true" : "false", libyang::CreationOptions::Update);
+    if (!is_cleared && (!existingAlarmNode || (existingAlarmNode && utils::childValue(*existingAlarmNode, "is-cleared") == "true"))) {
+        edit.newPath((alarmNodePath + "/last-raised").c_str(), utils::yangTimeFormat(now).c_str());
+    }
+
+    if (!is_cleared) {
+        edit.newPath((alarmNodePath + "/perceived-severity").c_str(), severity.c_str(), libyang::CreationOptions::Update);
+    }
+
+    edit.newPath((alarmNodePath + "/alarm-text").c_str(), std::string(input.findPath("alarm-text").value().asTerm().valueStr()).c_str(), libyang::CreationOptions::Update);
+
+    const auto& editAlarmNode = edit.findPath(alarmNodePath.c_str());
+    if (valueChanged(existingAlarmNode, *editAlarmNode, "alarm-text") || valueChanged(existingAlarmNode, *editAlarmNode, "is-cleared") || valueChanged(existingAlarmNode, *editAlarmNode, "perceived-severity")) {
+        edit.newPath((alarmNodePath + "/last-changed").c_str(), utils::yangTimeFormat(now).c_str());
+    }
+
+    m_log->trace("Update: {}", std::string(*edit.printStr(libyang::DataFormat::JSON, libyang::PrintFlags::Shrink)));
+    m_session.editBatch(edit, sysrepo::DefaultOperation::Merge);
+    m_session.applyChanges();
+
+    if (shouldNotifyStatusChange(m_session, existingAlarmNode, *editAlarmNode)) {
+        auto notification = createStatusChangeNotification(alarmNodePath);
+        m_session.sendNotification(notification, sysrepo::Wait::No);
+    }
+
+    return sysrepo::ErrorCode::Ok;
 }
 
 libyang::DataNode Daemon::createStatusChangeNotification(const std::string& alarmNodePath)