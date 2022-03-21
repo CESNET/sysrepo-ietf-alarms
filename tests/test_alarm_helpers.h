9eea9cce578f56e34516f86e159fa77c1ab55bb4
Tomáš Pecka <tomas.pecka@cesnet.cz>
2022-03-21 09:14:35 +0100

    tests: rework time interval testing
    The daemon writes to sysrepo current times. In testing, we have to
    handle that somehow (see a1d99f1833d293424cda88d482d6ce0fec3cb2de for
    rationale).
    However, the tests were testing whether the time is in the time
    interval between the RPC call and 300ms after. This time windows is too
    much benevolent and it effectively tested only whether the real time
    "was not before" the interval start (see previous commit for a bug
    caused by this).
    
    This has an easy fix. The RPCs are executed synchronously so let's store
    timepoints right before and right after the RPC invocation and expect
    that the times that were supposed to be changed by this RPC operation
    are in this interval.
    
    Change-Id: I64103a8f7b1e8b9448b7268f0abc0536aa5bad66

--- tests/test_alarm_helpers.h
+++ tests/test_alarm_helpers.h
@@ -7,27 +7,30 @@
 #pragma once
 #include <map>
 #include <string>
+#include <test_time_interval.h>
 
-#define CLIENT_ALARM_RPC(SESS, ID, QUALIFIER, RESOURCE, SEVERITY, TEXT) \
-    [&]() {                                                             \
-        auto inp = std::map<std::string, std::string>{                  \
-            {"resource", RESOURCE},                                     \
-            {"alarm-type-id", ID},                                      \
-            {"alarm-type-qualifier", QUALIFIER},                        \
-            {"severity", SEVERITY},                                     \
-            {"alarm-text", TEXT},                                       \
-        };                                                              \
-                                                                        \
-        auto time = std::chrono::system_clock::now();                   \
-        rpcFromSysrepo(*SESS, rpcPrefix, inp);                          \
-        return time;                                                    \
+#define CLIENT_ALARM_RPC(SESS, ID, QUALIFIER, RESOURCE, SEVERITY, TEXT)                                                          \
+    [&]() -> std::pair<std::chrono::time_point<std::chrono::system_clock>, std::chrono::time_point<std::chrono::system_clock>> { \
+        auto inp = std::map<std::string, std::string>{                                                                           \
+            {"resource", RESOURCE},                                                                                              \
+            {"alarm-type-id", ID},                                                                                               \
+            {"alarm-type-qualifier", QUALIFIER},                                                                                 \
+            {"severity", SEVERITY},                                                                                              \
+            {"alarm-text", TEXT},                                                                                                \
+        };                                                                                                                       \
+                                                                                                                                 \
+        auto intervalStart = std::chrono::system_clock::now();                                                                   \
+        rpcFromSysrepo(*SESS, rpcPrefix, inp);                                                                                   \
+        auto intervalEnd = std::chrono::system_clock::now();                                                                     \
+        return {intervalStart, intervalEnd};                                                                                     \
     }();
 
 #define CLIENT_PURGE_RPC(SESS, EXPECTED_NUMBER_OF_PURGED_ALARMS, CLEARANCE_STATUS, ADDITIONAL_PARAMS)                                                                    \
-    [&]() {                                                                                                                                                              \
+    [&]() -> std::pair<std::chrono::time_point<std::chrono::system_clock>, std::chrono::time_point<std::chrono::system_clock>> {                                         \
         auto inp = std::map<std::string, std::string> ADDITIONAL_PARAMS;                                                                                                 \
         inp["alarm-clearance-status"] = CLEARANCE_STATUS;                                                                                                                \
-        auto time = std::chrono::system_clock::now();                                                                                                                    \
+        auto intervalStart = std::chrono::system_clock::now();                                                                                                           \
         REQUIRE(rpcFromSysrepo(*SESS, purgeRpcPrefix, inp) == std::map<std::string, std::string>{{"/purged-alarms", std::to_string(EXPECTED_NUMBER_OF_PURGED_ALARMS)}}); \
-        return time;                                                                                                                                                     \
+        auto intervalEnd = std::chrono::system_clock::now();                                                                                                             \
+        return {intervalStart, intervalEnd};                                                                                                                             \
     }()
