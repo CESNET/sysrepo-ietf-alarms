ad41ce7129489c582562af7da37032697bdefcd9
Tomáš Pecka <tomas.pecka@cesnet.cz>
2022-03-29 16:27:41 +0200

    daemon: move alarms from shelf and back if configuration changes
    Change-Id: I86411a36c1031844c1be640ef63fd3aa086f8b0d

--- src/alarms/Daemon.h
+++ src/alarms/Daemon.h
@@ -12,13 +12,14 @@ public:
 private:
     sysrepo::Connection m_connection;
     sysrepo::Session m_session;
-    std::optional<sysrepo::Subscription> m_rpcSub;
+    std::optional<sysrepo::Subscription> m_alarmSub;
     std::optional<sysrepo::Subscription> m_inventorySub;
     alarms::Log m_log;
 
     sysrepo::ErrorCode submitAlarm(sysrepo::Session rpcSession, const libyang::DataNode& input);
     sysrepo::ErrorCode purgeAlarms(const libyang::DataNode& rpcInput, libyang::DataNode output);
     libyang::DataNode createStatusChangeNotification(const std::string& alarmNodePath);
+    void reshelve();
 };
 
 }
