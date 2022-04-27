CESNET/sysrepo-ietf-alarms

commit a6ddcb14046f0da1a5768d662efe2ebd1413840e

utils: check if sysrepo module has feature enabled
Change-Id: I245ee172ab478ee1a446a8e06a1b65236d2fd058

src/utils/sysrepo.cpp
@@ -60,6 +60,14 @@ void ensureModuleImplemented(const sysrepo::Session& session, const std::string&
     }
 }
 
+bool featureEnabled(const sysrepo::Session& session, const std::string& module, const std::string& revision, const std::string& feature)
+{
+    ensureModuleImplemented(session, module, revision);
+
+    auto mod = session.getContext().getModule(module, revision);
+    return mod->featureEnabled(feature);
+}
+
 void removeFromOperationalDS(::sysrepo::Connection connection, const std::vector<std::string>& removePaths)
 {
     auto log = spdlog::get("main");

src/utils/sysrepo.h
@@ -19,6 +19,7 @@ namespace alarms::utils {
 
 void initLogsSysrepo();
 void ensureModuleImplemented(const sysrepo::Session& session, const std::string& module, const std::string& revision);
+bool featureEnabled(const sysrepo::Session& session, const std::string& module, const std::string& revision, const std::string& feature);
 
 void removeFromOperationalDS(::sysrepo::Connection connection, const std::vector<std::string>& removePaths);
 }

commit 78f8cf578031d3338fc32cddb94665be628066a8

utils: merge helpers for ensuring that module and features are enabled
I misunderstood and thought that we are going to dynamically adjust to
allowed ietf-alarm features. Since we are not we can just check both
that module is installed and its required features are enabled.

Fixes: a6ddcb14046f0da1a5768d662efe2ebd1413840e
Change-Id: Ica2ed452fc8cc0de8f4e92eddf01667eaa40d5c9

src/utils/sysrepo.cpp
@@ -52,20 +52,20 @@ void initLogsSysrepo()
     sr_log_set_cb(spdlog_sr_log_cb);
 }
 
-/** @brief Checks whether a module is implemented in Sysrepo and throws if not. */
-void ensureModuleImplemented(const sysrepo::Session& session, const std::string& module, const std::string& revision)
+/** @brief Checks whether a module is implemented in Sysrepo and required features are enabled. Throws if not. */
+void ensureModuleImplemented(const sysrepo::Session& session, const std::string& module, const std::string& revision, const std::vector<std::string>& features)
 {
-    if (auto mod = session.getContext().getModule(module, revision); !mod || !mod->implemented()) {
+    auto mod = session.getContext().getModule(module, revision);
+
+    if (!mod || !mod->implemented()) {
         throw std::runtime_error(module + "@" + revision + " is not implemented in sysrepo.");
     }
-}
 
-bool featureEnabled(const sysrepo::Session& session, const std::string& module, const std::string& revision, const std::string& feature)
-{
-    ensureModuleImplemented(session, module, revision);
-
-    auto mod = session.getContext().getModule(module, revision);
-    return mod->featureEnabled(feature);
+    for (const auto& feature : features) {
+        if (!mod->featureEnabled(feature)) {
+            throw std::runtime_error("Feature " + feature + " not enabled for module " + module + "@" + revision);
+        }
+    }
 }
 
 void removeFromOperationalDS(::sysrepo::Connection connection, const std::vector<std::string>& removePaths)

src/utils/sysrepo.h
@@ -18,8 +18,7 @@ class Session;
 namespace alarms::utils {
 
 void initLogsSysrepo();
-void ensureModuleImplemented(const sysrepo::Session& session, const std::string& module, const std::string& revision);
-bool featureEnabled(const sysrepo::Session& session, const std::string& module, const std::string& revision, const std::string& feature);
+void ensureModuleImplemented(const sysrepo::Session& session, const std::string& module, const std::string& revision, const std::vector<std::string>& features = {});
 
 void removeFromOperationalDS(::sysrepo::Connection connection, const std::vector<std::string>& removePaths);
 }