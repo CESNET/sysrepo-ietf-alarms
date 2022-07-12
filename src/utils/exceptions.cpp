a2fe7b9b267b14b58c440ee9afa9466406c892c8
Tomáš Pecka <tomas.pecka@cesnet.cz>
2022-07-12 13:17:21 +0200

    utils: add fatal exception handler for journal
    Change-Id: I84f1eba89de764121b027f278f8dd7cdd8312573

---
+++ src/utils/exceptions.cpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2016-2019 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+ */
+
+#include <cxxabi.h>
+#include "utils/exceptions.h"
+#include "utils/log.h"
+
+namespace alarms::utils {
+/** @short Log that everything is screwed up and rethrow
+
+The purpose is to make sure that a nicely formatted error message gets stored into the journald buffer with a high enough priority.
+*/
+void fatalException [[noreturn]] (alarms::Log log, const std::exception& e, const std::string& when)
+{
+    int demangled;
+    auto classname = std::unique_ptr<char, decltype(&std::free)>(__cxxabiv1::__cxa_demangle(typeid(e).name(), nullptr, nullptr, &demangled), std::free);
+    log->critical("Fatal error in {}: {}", when, demangled == 0 ? classname.get() : typeid(e).name());
+    log->critical("{}", e.what());
+    throw;
+}
+}
