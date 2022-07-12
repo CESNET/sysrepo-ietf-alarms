2957ed90075665f376a44a0a97c8c284cfe1bb5d
Tomáš Pecka <tomas.pecka@cesnet.cz>
2022-07-12 12:32:40 +0200

    utils: introduce function that waits until signaled
    Change-Id: Id2b2a17dea0b1eb1e67920cc0478616f54ac2689

---
+++ src/utils/waitUntilSignalled.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Václav Kubernát <kubernat@cesnet.cz>
+ *
+ */
+
+namespace alarms::utils {
+void waitUntilSignaled();
+
+}
