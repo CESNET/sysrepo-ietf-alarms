CESNET/sysrepo-ietf-alarms

commit 1a0ff970dc7c2c6c410a20ff36e7fab3ee40bcb0

alarms: implement validate alarms against alarm-inventory
The ietf-alarms works with the idea that the list of alarms is
predetermined. Then, alarm operators can prepare a workflow for managing
all the alarms, because they know what alarms can appear in advance.
According to RFC 8632 the system MUST publish all possible alarm types
in the alarm-inventory container (see RFC 8632 [1, sec. 4.2]).

We have strictly implemented that. Any new alarm that is to be published
is rejected if the corresponding entry is not found in the
alarm-inventory.

[1] https://datatracker.ietf.org/doc/html/rfc8632

Change-Id: I4555f93fd26837f19b0e5020f5c8ac2dbee82d24

src/alarms/Daemon.cpp
@@ -162,6 +162,21 @@ std::optional<std::string> shouldBeShelved(sysrepo::Session session, const alarm
 
     return shelfName;
 }
+
+/** @brief Helper for validating alarm against alarm-inventory */
+bool validateInventoryField(const libyang::DataNode& node, const std::string& list, const std::string& searchFor)
+{
+    if (auto listNodes = node.findXPath(list); !listNodes.empty()) {
+        std::vector<std::string> elements;
+        std::transform(listNodes.begin(), listNodes.end(), std::back_inserter(elements), [&](const auto& node) { return std::string{node.asTerm().valueStr()}; });
+
+        if (auto it = std::find(elements.begin(), elements.end(), searchFor); it == elements.end()) {
+            return false;
+        }
+    }
+
+    return true;
+}
 }
 
 namespace alarms {
@@ -212,12 +227,58 @@ Daemon::Daemon()
         sysrepo::SubscribeOptions::DoneOnly | sysrepo::SubscribeOptions::Passive);
 }
 
+/** @brief Check whether published alarm is in alarm-inventory container
+ *
+ * According to RFC 8632 the system MUST publish all possible alarm types in the alarm inventory.
+ * This is to ensure that alarm operators know what alarms might appear.
+ *
+ * See RFC 8632, section 4.2
+ *
+ * @return optional<string> containing the error message if validation fails
+ * */
+std::optional<std::string> Daemon::inventoryValidationError(const Key& key, const std::string& severity)
+{
+    const std::string msgPrefix = "Published or cleared alarm id='" + key.alarmTypeId + "' qualifier='" + key.alarmTypeQualifier + "' resource='" + key.resource + "' severity='" + severity + "'";
+
+    auto data = m_session.getData(alarmInventoryPrefix);
+    assert(data);
+
+    const auto inventoryNodesXPath = alarmInventoryPrefix + "/alarm-type[alarm-type-id='"s + key.alarmTypeId + "'][alarm-type-qualifier='" + key.alarmTypeQualifier + "']";
+    auto inventoryNodes = data->findXPath(inventoryNodesXPath);
+    if (inventoryNodes.empty()) {
+        return msgPrefix + " but this alarm is not listed in the alarm inventory";
+    } else if (inventoryNodes.size() > 1) {
+        throw std::runtime_error("Internal error: too many matching alarms in alarm-inventory");
+    }
+
+    if (!validateInventoryField(*inventoryNodes.begin(), "resource", key.resource)) {
+        return msgPrefix + " but the resource is not listed in the alarm inventory for this alarm";
+    }
+
+    if (!validateInventoryField(*inventoryNodes.begin(), "severity-level", severity)) {
+        return msgPrefix + " but the severity is not listed in the alarm inventory for this alarm";
+    }
+
+    return std::nullopt;
+}
+
 sysrepo::ErrorCode Daemon::submitAlarm(sysrepo::Session rpcSession, const libyang::DataNode& input)
 {
     const auto& alarmKey = Key::fromNode(input);
     const auto severity = std::string(input.findPath("severity").value().asTerm().valueStr());
     const bool is_cleared = severity == "cleared";
     const auto now = std::chrono::system_clock::now();
+
+    if (auto inventoryError = inventoryValidationError(alarmKey, severity)) {
+        rpcSession.setNetconfError({.type = "application",
+                                    .tag = "data-missing",
+                                    .appTag = std::nullopt,
+                                    .path = std::nullopt,
+                                    .message = (inventoryError.value() + " Violation of RFC8632 (sec. 4.1).").c_str(),
+                                    .infoElements = {}});
+        m_log->warn(inventoryError.value());
+        return sysrepo::ErrorCode::OperationFailed;
+    }
 
     auto matchedShelf = shouldBeShelved(m_session, alarmKey);
     std::string alarmNodePath;

src/alarms/Daemon.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <optional>
 #include <sysrepo-cpp/Connection.hpp>
+#include "Key.h"
 #include "utils/log-fwd.h"
 
 namespace alarms {
@@ -19,6 +20,7 @@ private:
     sysrepo::ErrorCode submitAlarm(sysrepo::Session rpcSession, const libyang::DataNode& input);
     sysrepo::ErrorCode purgeAlarms(const std::string& rpcPath, const std::string& alarmListXPath, const libyang::DataNode& rpcInput, libyang::DataNode output);
     libyang::DataNode createStatusChangeNotification(const std::string& alarmNodePath);
+    std::optional<std::string> inventoryValidationError(const Key& key, const std::string& severity);
     void reshelve();
 };
 

tests/alarm_notifications.cpp
@@ -17,6 +17,7 @@ const auto ietfAlarmsModule = "ietf-alarms";
 const auto alarmStatusNotification = "/"s + ietfAlarmsModule + ":alarm-notification";
 const auto inventoryNotification = "/"s + ietfAlarmsModule + ":alarm-inventory-changed";
 const auto rpcPrefix = "/sysrepo-ietf-alarms:create-or-update-alarm";
+const auto alarmInventoryPrefix = "/ietf-alarms:alarms/alarm-inventory";
 }
 
 #define EXPECT_NOTIFICATION(PROPS) NAMED_REQUIRE_CALL(eventsAlarmStatus, notified(trompeloeil::eq(PROPS))).IN_SEQUENCE(seq1)
@@ -50,16 +51,6 @@ const auto rpcPrefix = "/sysrepo-ietf-alarms:create-or-update-alarm";
         lastChangedTimesInSysrepo.push_back(FETCH_TIME_CHANGED(ID, QUALIFIER, RESOURCE));       \
     }
 
-#define CLIENT_ALARM_INVENTORY(SESS, ID, QUALIFIER, WILL_CLEAR, DESCRIPTION, RESOURCES, SEVERITY_LEVELS)                                                         \
-    SESS->setItem("/ietf-alarms:alarms/alarm-inventory/alarm-type[alarm-type-id='" ID "'][alarm-type-qualifier='" QUALIFIER "']/will-clear", WILL_CLEAR);        \
-    SESS->setItem("/ietf-alarms:alarms/alarm-inventory/alarm-type[alarm-type-id='" ID "'][alarm-type-qualifier='" QUALIFIER "']/description", DESCRIPTION);      \
-    for (const auto& e : std::vector<std::string> RESOURCES) {                                                                                                   \
-        SESS->setItem("/ietf-alarms:alarms/alarm-inventory/alarm-type[alarm-type-id='" ID "'][alarm-type-qualifier='" QUALIFIER "']/resource", e.c_str());       \
-    }                                                                                                                                                            \
-    for (const auto& e : std::vector<std::string> SEVERITY_LEVELS) {                                                                                             \
-        SESS->setItem("/ietf-alarms:alarms/alarm-inventory/alarm-type[alarm-type-id='" ID "'][alarm-type-qualifier='" QUALIFIER "']/severity-level", e.c_str()); \
-    }
-
 TEST_CASE("Receiving alarm notifications")
 {
     TEST_SYSREPO_INIT_LOGS;
@@ -80,17 +71,19 @@ TEST_CASE("Receiving alarm notifications")
     std::vector<std::string> lastChangedTimesFromNotifications;
     std::mutex mtx;
 
-    NotificationWatcher eventsAlarmStatus(*userSess, alarmStatusNotification, [&mtx, &lastChangedTimesFromNotifications](const std::optional<libyang::DataNode>& tree) {
-        auto node = tree->findPath("time");
-        REQUIRE(node);
-        REQUIRE(node->isTerm());
-        std::lock_guard lck(mtx);
-        lastChangedTimesFromNotifications.emplace_back(node->asTerm().valueStr());
-    });
-    NotificationWatcher eventsInventory(*userSess, inventoryNotification);
-
     SECTION("Alarm status changes (/ietf-alarms:alarms/alarm-notification)")
     {
+        NotificationWatcher eventsAlarmStatus(*userSess, alarmStatusNotification, [&mtx, &lastChangedTimesFromNotifications](const std::optional<libyang::DataNode>& tree) {
+            auto node = tree->findPath("time");
+            REQUIRE(node);
+            REQUIRE(node->isTerm());
+            std::lock_guard lck(mtx);
+            lastChangedTimesFromNotifications.emplace_back(node->asTerm().valueStr());
+        });
+
+        CLIENT_INTRODUCE_ALARM(cli1Sess, "alarms-test:alarm-1", "", {}, {}, "");
+        CLIENT_INTRODUCE_ALARM(cli2Sess, "alarms-test:alarm-1", "", {}, {}, "");
+        CLIENT_INTRODUCE_ALARM(cli2Sess, "alarms-test:alarm-1", "qual", {}, {}, "");
         SECTION("notifications: all-status-changes")
         {
             CLIENT_ALARM_RPC_AND_EXPECT_NOTIFICATION(cli1Sess, "alarms-test:alarm-1", "", "edfa", "warning", "Hey, I'm overheating.");
@@ -150,12 +143,11 @@ TEST_CASE("Receiving alarm notifications")
 
     SECTION("Inventory status changes (/ietf-alarms:alarms/alarm-inventory-changed)")
     {
-        cli1Sess->switchDatastore(sysrepo::Datastore::Operational);
-        CLIENT_ALARM_INVENTORY(cli1Sess, "alarms-test:alarm-1", "high", "true", "Some description", {}, ({"minor"}));
-        CLIENT_ALARM_INVENTORY(cli1Sess, "alarms-test:alarm-2", "", "false", "Another description", ({"edfa", "wss"}), ({"minor", "major"}));
-        REQUIRE_CALL(eventsInventory, notified(NotificationWatcher::data_t{})).IN_SEQUENCE(seq1);
-        cli1Sess->applyChanges();
-        cli1Sess->switchDatastore(sysrepo::Datastore::Running);
+        NotificationWatcher eventsInventory(*userSess, inventoryNotification);
+
+        REQUIRE_CALL(eventsInventory, notified(NotificationWatcher::data_t{})).IN_SEQUENCE(seq1).TIMES(2);
+        CLIENT_INTRODUCE_ALARM(cli1Sess, "alarms-test:alarm-1", "high", {}, ({"minor"}), "Some description");
+        CLIENT_INTRODUCE_ALARM(cli1Sess, "alarms-test:alarm-2", "", ({"edfa", "wss"}), ({"minor", "major"}), "Another description");
 
         REQUIRE(dataFromSysrepo(*userSess, "/ietf-alarms:alarms/alarm-inventory", sysrepo::Datastore::Operational) == std::map<std::string, std::string>{
                     {"/alarm-type[alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='high']", ""},
@@ -172,14 +164,11 @@ TEST_CASE("Receiving alarm notifications")
                     {"/alarm-type[alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/resource[2]", "wss"},
                     {"/alarm-type[alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/severity-level[1]", "minor"},
                     {"/alarm-type[alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/severity-level[2]", "major"},
-                    {"/alarm-type[alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/will-clear", "false"},
+                    {"/alarm-type[alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/will-clear", "true"},
                 });
 
-        cli2Sess->switchDatastore(sysrepo::Datastore::Operational);
-        CLIENT_ALARM_INVENTORY(cli2Sess, "alarms-test:alarm-2-1", "", "false", "Another description", ({"edfa"}), {});
         REQUIRE_CALL(eventsInventory, notified(NotificationWatcher::data_t{})).IN_SEQUENCE(seq1);
-        cli2Sess->applyChanges();
-        cli2Sess->switchDatastore(sysrepo::Datastore::Running);
+        CLIENT_INTRODUCE_ALARM(cli2Sess, "alarms-test:alarm-2-1", "", ({"edfa"}), {}, "Another description");
 
         REQUIRE(dataFromSysrepo(*userSess, "/ietf-alarms:alarms/alarm-inventory", sysrepo::Datastore::Operational) == std::map<std::string, std::string>{
                     {"/alarm-type[alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='high']", ""},
@@ -196,13 +185,13 @@ TEST_CASE("Receiving alarm notifications")
                     {"/alarm-type[alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/resource[2]", "wss"},
                     {"/alarm-type[alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/severity-level[1]", "minor"},
                     {"/alarm-type[alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/severity-level[2]", "major"},
-                    {"/alarm-type[alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/will-clear", "false"},
+                    {"/alarm-type[alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/will-clear", "true"},
                     {"/alarm-type[alarm-type-id='alarms-test:alarm-2-1'][alarm-type-qualifier='']", ""},
                     {"/alarm-type[alarm-type-id='alarms-test:alarm-2-1'][alarm-type-qualifier='']/alarm-type-id", "alarms-test:alarm-2-1"},
                     {"/alarm-type[alarm-type-id='alarms-test:alarm-2-1'][alarm-type-qualifier='']/alarm-type-qualifier", ""},
                     {"/alarm-type[alarm-type-id='alarms-test:alarm-2-1'][alarm-type-qualifier='']/description", "Another description"},
                     {"/alarm-type[alarm-type-id='alarms-test:alarm-2-1'][alarm-type-qualifier='']/resource[1]", "edfa"},
-                    {"/alarm-type[alarm-type-id='alarms-test:alarm-2-1'][alarm-type-qualifier='']/will-clear", "false"},
+                    {"/alarm-type[alarm-type-id='alarms-test:alarm-2-1'][alarm-type-qualifier='']/will-clear", "true"},
                 });
 
         REQUIRE_CALL(eventsInventory, notified(NotificationWatcher::data_t{})).IN_SEQUENCE(seq1);
@@ -222,7 +211,7 @@ TEST_CASE("Receiving alarm notifications")
                     {"/alarm-type[alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/resource[2]", "wss"},
                     {"/alarm-type[alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/severity-level[1]", "minor"},
                     {"/alarm-type[alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/severity-level[2]", "major"},
-                    {"/alarm-type[alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/will-clear", "false"},
+                    {"/alarm-type[alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/will-clear", "true"},
                 });
 
         cli2Sess = TEST_INIT_SESSION;
@@ -241,7 +230,7 @@ TEST_CASE("Receiving alarm notifications")
                     {"/alarm-type[alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/resource[2]", "wss"},
                     {"/alarm-type[alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/severity-level[1]", "minor"},
                     {"/alarm-type[alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/severity-level[2]", "major"},
-                    {"/alarm-type[alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/will-clear", "false"},
+                    {"/alarm-type[alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']/will-clear", "true"},
                 });
 
         waitForCompletionAndBitMore(seq1);

tests/alarm_purge.cpp
@@ -19,6 +19,7 @@ const auto alarmList = "/ietf-alarms:alarms/alarm-list";
 const auto alarmListInstances = "/ietf-alarms:alarms/alarm-list/alarm";
 const auto shelvedAlarmList = "/ietf-alarms:alarms/shelved-alarms";
 const auto shelvedAlarmListInstances = "/ietf-alarms:alarms/shelved-alarms/shelved-alarm";
+const auto alarmInventoryPrefix = "/ietf-alarms:alarms/alarm-inventory";
 }
 
 bool includesAll(const std::map<std::string, std::string>& haystack, const PropsWithTimeTest& needles)
@@ -42,6 +43,9 @@ TEST_CASE("Purge alarms RPC")
 
     userSess->setItem("/ietf-alarms:alarms/control/alarm-shelving/shelf[name='shelf']/resource[.='wss']", std::nullopt);
     userSess->applyChanges();
+
+    CLIENT_INTRODUCE_ALARM(cli1Sess, "alarms-test:alarm-1", "", {}, {}, "Alarm 1");
+    CLIENT_INTRODUCE_ALARM(cli1Sess, "alarms-test:alarm-2", "", {}, {}, "Alarm 2");
 
     CLIENT_ALARM_RPC(cli1Sess, "alarms-test:alarm-1", "", "edfa", "warning", "A warning");
     CLIENT_ALARM_RPC(cli1Sess, "alarms-test:alarm-2", "", "edfa", "major", "A major issue");

tests/alarm_shelving.cpp
@@ -18,7 +18,7 @@ const auto ietfAlarms = "/ietf-alarms:alarms";
 const auto alarmListInstances = "/ietf-alarms:alarms/alarm-list/alarm";
 const auto shelvedAlarmsListInstances = "/ietf-alarms:alarms/shelved-alarms/shelved-alarm";
 const auto controlShelf = "/ietf-alarms:alarms/control/alarm-shelving/shelf";
-
+const auto alarmInventoryPrefix = "/ietf-alarms:alarms/alarm-inventory";
 
 struct ShelvedAlarm {
     std::string resource;
@@ -201,8 +201,27 @@ TEST_CASE("Alarm shelving")
     TEST_SYSREPO_CLIENT_INIT(cli2Sess);
     TEST_SYSREPO_CLIENT_INIT(userSess);
 
+    CLIENT_INTRODUCE_ALARM(userSess, "alarms-test:alarm-2-1", "high", {}, {}, "Alarm 1");
+    CLIENT_INTRODUCE_ALARM(userSess, "alarms-test:alarm-2-1", "low", {}, {}, "Alarm 1");
+    CLIENT_INTRODUCE_ALARM(userSess, "alarms-test:alarm-1", "high", {}, {}, "Alarm 1");
+
     REQUIRE(dataFromSysrepo(*userSess, "/ietf-alarms:alarms", sysrepo::Datastore::Operational) == PropsWithTimeTest{
                 {"/alarm-inventory", ""},
+                {"/alarm-inventory/alarm-type[alarm-type-id='alarms-test:alarm-2-1'][alarm-type-qualifier='high']", ""},
+                {"/alarm-inventory/alarm-type[alarm-type-id='alarms-test:alarm-2-1'][alarm-type-qualifier='high']/alarm-type-id", "alarms-test:alarm-2-1"},
+                {"/alarm-inventory/alarm-type[alarm-type-id='alarms-test:alarm-2-1'][alarm-type-qualifier='high']/alarm-type-qualifier", "high"},
+                {"/alarm-inventory/alarm-type[alarm-type-id='alarms-test:alarm-2-1'][alarm-type-qualifier='high']/description", "Alarm 1"},
+                {"/alarm-inventory/alarm-type[alarm-type-id='alarms-test:alarm-2-1'][alarm-type-qualifier='high']/will-clear", "true"},
+                {"/alarm-inventory/alarm-type[alarm-type-id='alarms-test:alarm-2-1'][alarm-type-qualifier='low']", ""},
+                {"/alarm-inventory/alarm-type[alarm-type-id='alarms-test:alarm-2-1'][alarm-type-qualifier='low']/alarm-type-id", "alarms-test:alarm-2-1"},
+                {"/alarm-inventory/alarm-type[alarm-type-id='alarms-test:alarm-2-1'][alarm-type-qualifier='low']/alarm-type-qualifier", "low"},
+                {"/alarm-inventory/alarm-type[alarm-type-id='alarms-test:alarm-2-1'][alarm-type-qualifier='low']/description", "Alarm 1"},
+                {"/alarm-inventory/alarm-type[alarm-type-id='alarms-test:alarm-2-1'][alarm-type-qualifier='low']/will-clear", "true"},
+                {"/alarm-inventory/alarm-type[alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='high']", ""},
+                {"/alarm-inventory/alarm-type[alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='high']/alarm-type-id", "alarms-test:alarm-1"},
+                {"/alarm-inventory/alarm-type[alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='high']/alarm-type-qualifier", "high"},
+                {"/alarm-inventory/alarm-type[alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='high']/description", "Alarm 1"},
+                {"/alarm-inventory/alarm-type[alarm-type-id='alarms-test:alarm-1'][alarm-type-qualifier='high']/will-clear", "true"},
                 {"/alarm-list", ""},
                 {"/alarm-list/number-of-alarms", "0"},
                 {"/alarm-list/last-changed", initTime},

tests/alarm_summary.cpp
@@ -13,6 +13,7 @@ namespace {
 
 const auto rpcPrefix = "/sysrepo-ietf-alarms:create-or-update-alarm";
 const auto purgeRpcPrefix = "/ietf-alarms:alarms/alarm-list/purge-alarms";
+const auto alarmInventoryPrefix = "/ietf-alarms:alarms/alarm-inventory";
 
 using summary_t = std::map<std::string, std::map<std::string, unsigned>>;
 
@@ -40,6 +41,9 @@ TEST_CASE("Basic alarm publishing and updating")
     auto daemon = std::make_unique<alarms::Daemon>();
 
     TEST_SYSREPO_CLIENT_INIT(userSess);
+
+    CLIENT_INTRODUCE_ALARM(userSess, "alarms-test:alarm-1", "high", {}, {}, "Alarm 1");
+    CLIENT_INTRODUCE_ALARM(userSess, "alarms-test:alarm-1", "blabla", {}, {}, "Alarm 1");
 
     REQUIRE(getSummary(*userSess) == summary_t{
                 {"indeterminate", {{"total", 0}, {"cleared", 0}, {"not-cleared", 0}}},

tests/test_alarm_helpers.h
@@ -8,6 +8,7 @@
 #include <map>
 #include <string>
 #include <test_time_interval.h>
+#include "utils/sysrepo.h"
 
 #define CLIENT_ALARM_RPC(SESS, ID, QUALIFIER, RESOURCE, SEVERITY, TEXT) \
     [&]() {                                                             \
@@ -24,6 +25,21 @@
         auto intervalEnd = std::chrono::system_clock::now();            \
         return AnyTimeBetween{intervalStart, intervalEnd};              \
     }();
+
+#define CLIENT_INTRODUCE_ALARM(SESS, ID, QUALIFIER, RESOURCES, SEVERITIES, DESCRIPTION)                                                                                              \
+    {                                                                                                                                                                                \
+        alarms::utils::ScopedDatastoreSwitch s(*SESS, sysrepo::Datastore::Operational);                                                                                              \
+                                                                                                                                                                                     \
+        SESS->setItem(std::string{alarmInventoryPrefix} + "/alarm-type[alarm-type-id='" + ID + "'][alarm-type-qualifier='" + QUALIFIER + "']/description", DESCRIPTION);             \
+        SESS->setItem(std::string{alarmInventoryPrefix} + "/alarm-type[alarm-type-id='" + ID + "'][alarm-type-qualifier='" + QUALIFIER + "']/will-clear", "true");                   \
+        for (const auto& resource : std::vector<std::string> RESOURCES) {                                                                                                            \
+            SESS->setItem(std::string{alarmInventoryPrefix} + "/alarm-type[alarm-type-id='" + ID + "'][alarm-type-qualifier='" + QUALIFIER + "']/resource", resource.c_str());       \
+        }                                                                                                                                                                            \
+        for (const auto& severity : std::vector<std::string> SEVERITIES) {                                                                                                           \
+            SESS->setItem(std::string{alarmInventoryPrefix} + "/alarm-type[alarm-type-id='" + ID + "'][alarm-type-qualifier='" + QUALIFIER + "']/severity-level", severity.c_str()); \
+        }                                                                                                                                                                            \
+        SESS->applyChanges();                                                                                                                                                        \
+    }
 
 #define CLIENT_PURGE_RPC_IMPL(SESS, RPCPATH, EXPECTED_NUMBER_OF_PURGED_ALARMS, CLEARANCE_STATUS, ADDITIONAL_PARAMS)                                               \
     [&]() {                                                                                                                                                       \

commit 2b99fa9e1762d3cda63f3b9e47be127ee7c93147

daemon: don't validate severity=cleared against alarm-inventory
Severity-level=cleared is not a severity-level from ietf-alarms model
but our extension that allows us to make alarm clearing simple. However,
when someone clears an alarm by setting severity-level=cleared, we were
validating if this severity-level is valid for given alarm.

Fixes: 1a0ff970dc7c2c6c410a20ff36e7fab3ee40bcb0
Change-Id: I237f3a4ec3b8c1b246b133b2e948f73865fc91d8

src/alarms/Daemon.cpp
@@ -255,7 +255,7 @@ std::optional<std::string> Daemon::inventoryValidationError(const Key& key, cons
         return msgPrefix + " but the resource is not listed in the alarm inventory for this alarm";
     }
 
-    if (!validateInventoryField(*inventoryNodes.begin(), "severity-level", severity)) {
+    if (severity != "cleared" && !validateInventoryField(*inventoryNodes.begin(), "severity-level", severity)) {
         return msgPrefix + " but the severity is not listed in the alarm inventory for this alarm";
     }
 

tests/alarm_publish.cpp
@@ -908,5 +908,6 @@ TEST_CASE("Basic alarm publishing and updating")
         REQUIRE_THROWS_WITH([&]() { CLIENT_ALARM_RPC(cli1Sess, "alarms-test:alarm-2-2", "", "invalid-resource", "major", "Invalid resource") }(), "Couldn't send RPC: SR_ERR_CALLBACK_FAILED");
         REQUIRE_THROWS_WITH([&]() { CLIENT_ALARM_RPC(cli1Sess, "alarms-test:alarm-2-2", "", "another-resource", "indeterminate", "Invalid severity") }(), "Couldn't send RPC: SR_ERR_CALLBACK_FAILED");
         CLIENT_ALARM_RPC(cli1Sess, "alarms-test:alarm-2-2", "", "another-resource", "critical", "valid");
+        CLIENT_ALARM_RPC(cli1Sess, "alarms-test:alarm-2-2", "", "another-resource", "cleared", "valid");
     }
 }