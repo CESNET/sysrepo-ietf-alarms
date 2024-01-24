CESNET/sysrepo-ietf-alarms

commit cbddff3dcad568fdcf8b5d5d70e4696c8b4295dd

statistics: maintain cache of all alarms
This commit introduces a local cache which holds "all alarms", no matter
whether they are supposed to be shelved or not, and whether they have
been already cleared or if they are still active. This cache is then
used for creating statistics without re-fetching the data from sysrepo.
This avoids a read/modify/write cycle of the entire operational DS for
this module, and compared to the previous version, there's an
incremental speedup of 43%.

The data are still being read each time an alarm is updated. This will
be removed in some future commit when the alarm handling code will be
only pushing stuff to sysrepo, without reading the data all the time.

The change in the test suite looks valid to me. On line 85, the test
clears an alarm that's currently shelved. Because such as change is
visible in the `shelved-alarms/shelved-alarm` list, I believe that doing
that should touch the `shelved-alarms-last-changed` leaf. Previously,
the test actually checked that the timestamp is set to *before* that
last RPC (which clears that shelved alarm) was triggered.

Change-Id: Ie40a852d1605f55a41e307059737251fb26cf9cc

src/alarms/Daemon.cpp
@@ -18,9 +18,9 @@ namespace {
 const auto rootPath = "/ietf-alarms:alarms"s;
 const auto rpcPrefix = "/sysrepo-ietf-alarms:create-or-update-alarm";
 const auto ietfAlarmsModule = "ietf-alarms";
-const auto alarmList = "/ietf-alarms:alarms/alarm-list";
+const auto alarmList = "/ietf-alarms:alarms/alarm-list"s;
 const auto alarmListInstances = "/ietf-alarms:alarms/alarm-list/alarm";
-const auto shelvedAlarmList = "/ietf-alarms:alarms/shelved-alarms";
+const auto shelvedAlarmList = "/ietf-alarms:alarms/shelved-alarms"s;
 const auto shelvedAlarmListInstances = "/ietf-alarms:alarms/shelved-alarms/shelved-alarm";
 const auto purgeRpcPrefix = "/ietf-alarms:alarms/alarm-list/purge-alarms";
 const auto purgeShelvedRpcPrefix = "/ietf-alarms:alarms/shelved-alarms/purge-shelved-alarms";
@@ -41,67 +41,6 @@ const std::array Severities{
     "major",
     "critical",
 };
-
-/** @brief returns number of list instances in the list specified by xPath */
-size_t numberOfListInstances(const libyang::DataNode& alarmRoot, const std::string& xPath)
-{
-    return alarmRoot.findXPath(xPath).size();
-}
-
-void updateStats(libyang::DataNode& edit, const std::string& prefix, const std::string& alarmsCountLeafName, size_t alarmCount, const std::string& lastChangedLeafName, const std::chrono::time_point<std::chrono::system_clock>& lastChanged)
-{
-    // number-of-alarms is of type yang:gauge32. If we ever support more than 2^32-1 alarms then we will have to deal with cropping the value.
-    edit.newPath(prefix + "/" + alarmsCountLeafName, std::to_string(alarmCount));
-    edit.newPath(prefix + "/" + lastChangedLeafName, alarms::utils::yangTimeFormat(lastChanged));
-}
-
-void updateAlarmListStats(libyang::DataNode& edit, size_t alarmCount, const std::chrono::time_point<std::chrono::system_clock>& lastChanged)
-{
-    updateStats(edit, alarmList, "number-of-alarms", alarmCount, "last-changed", lastChanged);
-}
-
-void updateShelvedAlarmListStats(libyang::DataNode& edit, size_t alarmCount, const std::chrono::time_point<std::chrono::system_clock>& lastChanged)
-{
-    updateStats(edit, shelvedAlarmList, "number-of-shelved-alarms", alarmCount, "shelved-alarms-last-changed", lastChanged);
-}
-
-void updateAlarmSummary(sysrepo::Session session)
-{
-    struct SeveritySummary {
-        unsigned total;
-        unsigned cleared;
-    };
-    using AlarmSummary = std::map<std::string, SeveritySummary>;
-
-    auto alarmRoot = session.getData(rootPath);
-    assert(alarmRoot);
-
-    AlarmSummary alarmSummary;
-    for (const auto& severity : std::span{Severities.begin() + 1 /* dummy value */ + 1 /* cleared */, Severities.end()}) {
-        alarmSummary[severity];
-    }
-
-    for (const auto& alarmNode : alarmRoot->findXPath(alarmListInstances)) {
-        const auto severity = alarms::utils::childValue(alarmNode, "perceived-severity");
-        const auto isCleared = alarms::utils::childValue(alarmNode, "is-cleared");
-
-        alarmSummary[severity].total++;
-        if (isCleared == "true") {
-            alarmSummary[severity].cleared++;
-        }
-    }
-
-    auto edit = session.getContext().newPath(alarmSummaryPrefix);
-    for (const auto& [severity, summ] : alarmSummary) {
-        const auto prefix = alarmSummaryPrefix + "/alarm-summary[severity='"s + severity + "']";
-        edit.newPath(prefix + "/total", std::to_string(summ.total));
-        edit.newPath(prefix + "/not-cleared", std::to_string(summ.total - summ.cleared));
-        edit.newPath(prefix + "/cleared", std::to_string(summ.cleared));
-    }
-
-    session.editBatch(edit, sysrepo::DefaultOperation::Merge);
-    session.applyChanges();
-}
 
 bool valueChanged(const std::optional<libyang::DataNode>& oldNode, const libyang::DataNode& newNode, const char* leafName)
 {
@@ -172,19 +111,17 @@ Daemon::Daemon()
     , m_log(spdlog::get("main"))
     , m_notifyStatusChanges(NotifyStatusChanges::All)
     , m_inventoryDirty(true)
+    , m_alarmListLastChanged(TimePoint::clock::now())
+    , m_shelfListLastChanged(TimePoint::clock::now())
 {
     utils::ensureModuleImplemented(m_session, ietfAlarmsModule, "2019-09-11", {"alarm-shelving", "alarm-summary"});
     utils::ensureModuleImplemented(m_session, "sysrepo-ietf-alarms", "2022-02-17");
 
     {
         auto edit = m_session.getContext().newPath(alarmList);
-        auto now = std::chrono::system_clock::now();
-        updateAlarmListStats(edit, 0, now);
-        updateShelvedAlarmListStats(edit, 0, now);
+        updateStatistics(edit);
         m_session.editBatch(edit, sysrepo::DefaultOperation::Merge);
         m_session.applyChanges();
-
-        updateAlarmSummary(m_session);
     }
 
     m_alarmSub = m_session.onRPCAction(rpcPrefix, [&](sysrepo::Session session, auto, auto, const libyang::DataNode input, auto, auto, auto) {
@@ -295,10 +232,10 @@ std::optional<std::string> Daemon::inventoryValidationError(const InstanceKey& k
 
 sysrepo::ErrorCode Daemon::submitAlarm(sysrepo::Session rpcSession, const libyang::DataNode& input)
 {
+    const auto now = TimePoint::clock::now();
     const auto& alarmKey = InstanceKey::fromNode(input);
     const auto severity = std::get<libyang::Enum>(input.findPath("severity").value().asTerm().value()).value;
     const bool is_cleared = severity == ClearedSeverity;
-    const auto now = std::chrono::system_clock::now();
 
     const auto alarmRoot = m_session.getData(rootPath);
     assert(alarmRoot);
@@ -339,6 +276,7 @@ sysrepo::ErrorCode Daemon::submitAlarm(sysrepo::Session rpcSession, const libyan
     }
 
     const auto existingAlarmNode = alarmRoot->findPath(alarmNodePath);
+    auto& alarm = m_alarms[alarmKey];
 
     auto edit = m_session.getContext().newPath(alarmNodePath, std::nullopt, libyang::CreationOptions::Update);
 
@@ -347,39 +285,41 @@ sysrepo::ErrorCode Daemon::submitAlarm(sysrepo::Session rpcSession, const libyan
         return sysrepo::ErrorCode::Ok;
     } else if (!existingAlarmNode && !matchedShelf) {
         edit.newPath(alarmNodePath + "/time-created", utils::yangTimeFormat(now));
+        alarm.created = now;
     }
 
     edit.newPath(alarmNodePath + "/is-cleared", is_cleared ? "true" : "false", libyang::CreationOptions::Update);
+    alarm.isCleared = is_cleared;
     if (!is_cleared && (!existingAlarmNode || (existingAlarmNode && utils::childValue(*existingAlarmNode, "is-cleared") == "true"))) {
         edit.newPath(alarmNodePath + "/last-raised", utils::yangTimeFormat(now));
+        alarm.lastRaised = now;
     }
 
     if (!is_cleared) {
         edit.newPath(alarmNodePath + "/perceived-severity", Severities[severity], libyang::CreationOptions::Update);
+        alarm.lastSeverity = severity;
     }
 
     edit.newPath(alarmNodePath + "/alarm-text", std::string{input.findPath("alarm-text").value().asTerm().valueStr()}, libyang::CreationOptions::Update);
+    alarm.text = std::string{input.findPath("alarm-text").value().asTerm().valueStr()};
 
     const auto& editAlarmNode = edit.findPath(alarmNodePath);
     if (valueChanged(existingAlarmNode, *editAlarmNode, "alarm-text") || valueChanged(existingAlarmNode, *editAlarmNode, "is-cleared") || valueChanged(existingAlarmNode, *editAlarmNode, "perceived-severity")) {
         edit.newPath(alarmNodePath + "/last-changed", utils::yangTimeFormat(now));
+        alarm.lastChanged = now;
+    }
+
+    if (matchedShelf) {
+        edit.newPath(alarmNodePath + "/shelf-name", matchedShelf);
+        alarm.shelf = matchedShelf;
     }
 
     m_log->trace("Update: {}", std::string(*edit.printStr(libyang::DataFormat::JSON, libyang::PrintFlags::Shrink)));
 
-    if (!matchedShelf) {
-        updateAlarmListStats(edit, numberOfListInstances(*alarmRoot, alarmListInstances) + static_cast<int>(editAlarmNode->findPath("time-created").has_value()), now);
-    } else {
-        edit.newPath(alarmNodePath + "/shelf-name", matchedShelf);
-        if (!existingAlarmNode) {
-            updateShelvedAlarmListStats(edit, numberOfListInstances(*alarmRoot, shelvedAlarmListInstances) + 1, now);
-        }
-    }
+    updateStatistics(edit);
 
     m_session.editBatch(edit, sysrepo::DefaultOperation::Merge);
     m_session.applyChanges();
-
-    updateAlarmSummary(m_session);
 
     if (shouldNotifyStatusChange(existingAlarmNode, *editAlarmNode, m_notifyStatusChanges, m_notifySeverityThreshold)) {
         m_session.sendNotification(createStatusChangeNotification(edit.findPath(alarmNodePath).value()), sysrepo::Wait::No);
@@ -418,23 +358,21 @@ sysrepo::ErrorCode Daemon::purgeAlarms(const std::string& rpcPath, const std::st
     for (const auto& alarmNode : alarmRoot->findXPath(alarmListXPath)) {
         if (filter.matches(alarmNode)) {
             toDelete.push_back(std::string(alarmNode.path()));
+            m_alarms.erase(InstanceKey::fromNode(alarmNode));
         }
     }
 
     if (!toDelete.empty()) {
         auto edit = m_session.getContext().newPath(alarmList);
-
         if (rpcPath == purgeRpcPrefix) {
-            updateAlarmListStats(edit, numberOfListInstances(*alarmRoot, alarmListInstances) - toDelete.size(), now);
+            m_alarmListLastChanged = now;
         } else {
-            updateShelvedAlarmListStats(edit, numberOfListInstances(*alarmRoot, shelvedAlarmListInstances) - toDelete.size(), now);
+            m_shelfListLastChanged = now;
         }
-
+        updateStatistics(edit);
         utils::removeFromOperationalDS(m_session.getContext(), edit, toDelete);
         m_session.editBatch(edit, sysrepo::DefaultOperation::Merge);
         m_session.applyChanges();
-
-        updateAlarmSummary(m_session);
     }
 
     output.newPath(rpcPath + "/purged-alarms", std::to_string(toDelete.size()), libyang::CreationOptions::Output);
@@ -477,9 +415,6 @@ void Daemon::reshelve()
     auto now = std::chrono::system_clock::now();
     std::vector<std::string> toErase;
     bool change = false;
-    bool movedBetweenShelfs = false;
-    size_t shelvedCount = 0;
-    size_t unshelvedCount = 0;
 
     for (const auto& node : alarmRoot->findXPath(alarmListInstances)) {
         const auto alarmKey = InstanceKey::fromNode(node);
@@ -487,8 +422,10 @@ void Daemon::reshelve()
             createShelvedAlarmNodeFromExistingNode(edit, node, alarmKey, *shelf);
             m_log->trace("Alarm {} shelved ({})", node.path(), *shelf);
             toErase.emplace_back(node.path());
-            shelvedCount += 1;
             change = true;
+            m_alarms[alarmKey].shelf = shelf;
+            m_alarmListLastChanged = now;
+            m_shelfListLastChanged = now;
         }
     }
 
@@ -499,30 +436,25 @@ void Daemon::reshelve()
                 edit.newPath(shelvedAlarmListInstances + alarmKey.xpathIndex() + "/shelf-name", *shelf);
                 m_log->trace("Alarm {} moved between shelfs ({} -> {})", node.path(), utils::childValue(node, "shelf-name"), *shelf);
                 change = true;
-                movedBetweenShelfs = true;
+                m_alarms[alarmKey].shelf = shelf;
+                m_shelfListLastChanged = now;
             }
         } else {
             createAlarmNodeFromExistingNode(edit, node, alarmKey, now);
             m_log->trace("Alarm {} moved from shelf", node.path());
             toErase.emplace_back(node.path());
-            unshelvedCount += 1;
             change = true;
+            m_alarms[alarmKey].shelf = std::nullopt;
+            m_alarmListLastChanged = now;
+            m_shelfListLastChanged = now;
         }
     }
 
     if (change) {
-        if (shelvedCount > 0 || unshelvedCount > 0) {
-            updateAlarmListStats(edit, numberOfListInstances(*alarmRoot, alarmListInstances) - shelvedCount + unshelvedCount, now);
-        }
-        if (shelvedCount > 0 || unshelvedCount > 0 || movedBetweenShelfs) {
-            updateShelvedAlarmListStats(edit, numberOfListInstances(*alarmRoot, shelvedAlarmListInstances) + shelvedCount - unshelvedCount, now);
-        }
-
+        updateStatistics(edit);
         utils::removeFromOperationalDS(m_session.getContext(), edit, toErase);
         m_session.editBatch(edit, sysrepo::DefaultOperation::Merge);
         m_session.applyChanges();
-
-        updateAlarmSummary(m_session);
     }
 }
 
@@ -552,5 +484,55 @@ void Daemon::rebuildInventory(const libyang::DataNode& dataWithInventory)
                 });
         }
     }
+}
+
+void Daemon::updateStatistics(libyang::DataNode& edit)
+{
+    struct PerSeveritySummary {
+        unsigned total;
+        unsigned cleared;
+    };
+    std::map<int32_t, PerSeveritySummary> summary;
+    int32_t totalList{0}, totalShelved{0};
+
+    // make sure that all severities are present *and* initialize counters to zero
+    for (unsigned i = 2 /* #0: dummy, #1: cleared, #2: the first real one */; i < Severities.size(); ++i) {
+        summary[i] = PerSeveritySummary{
+            .total = 0,
+            .cleared = 0,
+        };
+    }
+
+    for (const auto& [key, entry] : m_alarms) {
+        if (entry.shelf) {
+            // shelved alarms do not affect the alarm-summary
+            ++totalShelved;
+            if (entry.lastChanged >= m_shelfListLastChanged) {
+                m_shelfListLastChanged = entry.lastChanged;
+            }
+            continue;
+        }
+
+        ++totalList;
+        ++summary[entry.lastSeverity].total;
+        if (entry.isCleared) {
+            ++summary[entry.lastSeverity].cleared;
+        }
+        if (entry.lastChanged >= m_alarmListLastChanged) {
+            m_alarmListLastChanged = entry.lastChanged;
+        }
+    }
+
+    for (const auto& [severity, summ] : summary) {
+        const auto prefix = alarmSummaryPrefix + "/alarm-summary[severity='"s + Severities[severity] + "']";
+        edit.newPath(prefix + "/total", std::to_string(summ.total));
+        edit.newPath(prefix + "/not-cleared", std::to_string(summ.total - summ.cleared));
+        edit.newPath(prefix + "/cleared", std::to_string(summ.cleared));
+    }
+
+    edit.newPath(alarmList + "/number-of-alarms", std::to_string(totalList));
+    edit.newPath(alarmList + "/last-changed", utils::yangTimeFormat(m_alarmListLastChanged));
+    edit.newPath(shelvedAlarmList + "/number-of-shelved-alarms", std::to_string(totalShelved));
+    edit.newPath(shelvedAlarmList + "/shelved-alarms-last-changed", utils::yangTimeFormat(m_shelfListLastChanged));
 }
 }

src/alarms/Daemon.h
@@ -8,6 +8,17 @@
 #include "utils/log-fwd.h"
 
 namespace alarms {
+using TimePoint = std::chrono::time_point<std::chrono::system_clock>;
+
+struct AlarmEntry {
+    TimePoint created;
+    TimePoint lastRaised;
+    TimePoint lastChanged;
+    std::string text;
+    std::optional<std::string> shelf;
+    int32_t lastSeverity;
+    bool isCleared;
+};
 
 class Daemon {
 public:
@@ -36,6 +47,8 @@ private:
     std::mutex m_mtx;
     bool m_inventoryDirty;
     std::unordered_map<Type, InventoryData, boost::hash<Type>> m_inventory;
+    std::unordered_map<InstanceKey, AlarmEntry, boost::hash<InstanceKey>> m_alarms;
+    TimePoint m_alarmListLastChanged, m_shelfListLastChanged;
 
     sysrepo::ErrorCode submitAlarm(sysrepo::Session rpcSession, const libyang::DataNode& input);
     sysrepo::ErrorCode purgeAlarms(const std::string& rpcPath, const std::string& alarmListXPath, const libyang::DataNode& rpcInput, libyang::DataNode output);
@@ -43,6 +56,7 @@ private:
     std::optional<std::string> inventoryValidationError(const InstanceKey& key, const int32_t severity);
     void reshelve();
     void rebuildInventory(const libyang::DataNode& dataWithInventory);
+    void updateStatistics(libyang::DataNode& edit);
 };
 
 }

src/alarms/Key.h
@@ -45,5 +45,15 @@ struct InstanceKey {
 
     std::string xpathIndex() const;
     static InstanceKey fromNode(const libyang::DataNode& node);
+    bool operator==(const InstanceKey& other) const = default;
 };
+
+inline std::size_t hash_value(const InstanceKey& k)
+{
+    std::size_t seed = 0;
+    boost::hash_combine(seed, k.type);
+    boost::hash_combine(seed, k.resource);
+    return seed;
+
+}
 }

tests/alarm_purge.cpp
@@ -88,7 +88,7 @@ TEST_CASE("Purge alarms RPC")
                 "/ietf-alarms:alarms/shelved-alarms/shelved-alarm[resource='wss'][alarm-type-id='alarms-test:alarm-2'][alarm-type-qualifier='']",
             });
     REQUIRE(includesAll(dataFromSysrepo(*userSess, shelvedAlarmList, sysrepo::Datastore::Operational), PropsWithTimeTest{
-                                                                                                           {"/shelved-alarms-last-changed", BEFORE_INTERVAL(timeShelf)},
+                                                                                                           {"/shelved-alarms-last-changed", timeShelf},
                                                                                                            {"/number-of-shelved-alarms", "2"},
                                                                                                        }));
 

commit 5f29a041f7ed4f2edbfef0b67067fd828cbaff5c

avoid AlarmEntry implicit construction in the cache
std::map::operator[](const Key&) default-constructs or zero-initializes
when the key is not present [1].
This is what we use for getting alarms from m_alarms cache. However,
when the alarm was cleared, then the function returned in the next few
lines and left the alarm zero-initialized in the m_alarms map.

This had an unfortunate consequence when somebody called the alarm RPC
with an cleared alarm (this had to be the first call with such key) and
then updateStatistics was triggered, there was an alarm in the cache
with lastSeverity=0 because of the zero-initialization.

And that was the full story behind this error seen in our velia CI logs.

  libyang[0]: Invalid enumeration value "_". (path: Schema location "/ietf-alarms:alarms/summary/alarm-summary/severity".)

[1] https://en.cppreference.com/w/cpp/container/map/operator_at

Fixes: cbddff3dcad568fdcf8b5d5d70e4696c8b4295dd
Change-Id: I82fee8c300244902081c93d3be510a5fbd92ba60

src/alarms/Daemon.cpp
@@ -276,14 +276,17 @@ sysrepo::ErrorCode Daemon::submitAlarm(sysrepo::Session rpcSession, const libyan
     }
 
     const auto existingAlarmNode = alarmRoot->findPath(alarmNodePath);
-    auto& alarm = m_alarms[alarmKey];
 
     auto edit = m_session.getContext().newPath(alarmNodePath, std::nullopt, libyang::CreationOptions::Update);
 
     if (is_cleared && (!existingAlarmNode || (existingAlarmNode && utils::childValue(*existingAlarmNode, "is-cleared") == "true"))) {
         // if passing is-cleared=true the alarm either doesn't exist or exists but is inactive (is-cleared=true), do nothing, it's a NOOP
         return sysrepo::ErrorCode::Ok;
-    } else if (!existingAlarmNode && !matchedShelf) {
+    }
+
+    auto& alarm = m_alarms[alarmKey];
+
+    if (!existingAlarmNode && !matchedShelf) {
         edit.newPath(alarmNodePath + "/time-created", utils::yangTimeFormat(now));
         alarm.created = now;
     }

tests/alarm_publish.cpp
@@ -1042,4 +1042,14 @@ TEST_CASE("Netopeer2 clients can't publish alarms")
                     {"/alarm-summary[severity='warning']/total", "1"},
                 });
     }
+
+    SECTION("First action for an alarm is 'cleared'")
+    {
+        CLIENT_INTRODUCE_ALARM(cliSess, "alarms-test:alarm-1", "foo", {}, {}, "test1");
+        CLIENT_INTRODUCE_ALARM(cliSess, "alarms-test:alarm-2-1", "bar", {}, {}, "test2");
+
+        CLIENT_ALARM_RPC(cliSess, "alarms-test:alarm-1", "foo", "", "cleared", "High temperature on any resource with any severity");
+        CLIENT_ALARM_RPC(cliSess, "alarms-test:alarm-2-1", "bar", "", "minor", "Alarm with specific severity and resource");
+        auto actualDataFromSysrepo = dataFromSysrepo(*cliSess, "/ietf-alarms:alarms/summary", sysrepo::Datastore::Operational);
+    }
 }