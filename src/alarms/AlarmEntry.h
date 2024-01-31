cc964d08fc34178512a45da53374d1b688fa4a86
Jan Kundrát <jan.kundrat@cesnet.cz>
2024-01-31 20:36:37 +0100

    alarms: do not fetch data when updating alarms
    Change-Id: I904dc798197290596c3e6227b477786f3e771a88

--- src/alarms/AlarmEntry.h
+++ src/alarms/AlarmEntry.h
@@ -1,10 +1,17 @@
 #pragma once
 #include <chrono>
+#include <libyang-cpp/DataNode.hpp>
 #include <optional>
 #include <string>
 
 namespace alarms {
 using TimePoint = std::chrono::time_point<std::chrono::system_clock>;
+
+enum class NotifyStatusChanges {
+    All,
+    RaiseAndClear,
+    BySeverity,
+};
 
 struct AlarmEntry {
     TimePoint created;
@@ -14,5 +21,18 @@ struct AlarmEntry {
     std::optional<std::string> shelf;
     int32_t lastSeverity;
     bool isCleared;
+
+    struct WhatChanged {
+        bool changed;
+        bool shouldNotify;
+    };
+
+    WhatChanged updateByRpc(
+        const bool wasPresent,
+        const TimePoint now,
+        const libyang::DataNode& input,
+        const std::optional<std::string> shelf,
+        const NotifyStatusChanges notifyStatusChanges,
+        const std::optional<int32_t> notifySeverityThreshold);
 };
 }
