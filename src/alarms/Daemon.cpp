CESNET/sysrepo-ietf-alarms

commit 2d15c1c49a78a5d4ad32278aa17593b372692f09

lazy population of alarm inventory cache
The parent commit introduced an annoying performance hog into the code
path where alarm inventory entries were processed. That was done in the
name of a possible future speedup of "regular" alarm handling. The
bottleneck was actually in that dreaded read from sysrepo; the code
would have to load and parse the module's operational DS content each
time the inventory changed.

Considering that the original issue was that "pushing inventory entries
is slow", making it even slower is not good at all. Solve that by *not*
populating the inventory cache until it's really needed. This speeds up
the code by about 6%, which makes it almost on par with how it was when
no inventory cache was maintained.

Change-Id: I9ce5a5bce9025950e4a0257272baad85f7015b56

src/alarms/Daemon.cpp
@@ -159,6 +159,7 @@ Daemon::Daemon()
     , m_session(m_connection.sessionStart(sysrepo::Datastore::Operational))
     , m_log(spdlog::get("main"))
     , m_notifyStatusChanges(NotifyStatusChanges::All)
+    , m_inventoryDirty(true)
 {
     utils::ensureModuleImplemented(m_session, ietfAlarmsModule, "2019-09-11", {"alarm-shelving", "alarm-summary"});
     utils::ensureModuleImplemented(m_session, "sysrepo-ietf-alarms", "2022-02-17");
@@ -240,33 +241,11 @@ Daemon::Daemon()
     }
 
     m_inventorySub = m_session.onModuleChange(
-        ietfAlarmsModule, [&](auto session, auto, auto, auto, auto, auto) {
-            std::unique_lock lck{m_mtx};
-            const auto data = session.getData(alarmInventoryPrefix)->findPath(alarmInventoryPrefix);
-            m_inventory.clear();
-            if (data->child()) {
-                for (const auto& entry : data->child()->siblings()) {
-                    decltype(InventoryData::resources) resources;
-                    decltype(InventoryData::severities) severities;
-                    for (const auto& child : entry.immediateChildren()) {
-                        const auto shortName = child.schema().name();
-                        if (shortName == "resource") {
-                            resources.emplace(child.asTerm().valueStr());
-                        } else if (shortName == "severity-level") {
-                            severities.emplace(child.asTerm().valueStr());
-                        }
-                    }
-                    m_inventory.emplace(
-                        Type{
-                            .id = utils::childValue(entry, "alarm-type-id"),
-                            .qualifier = utils::childValue(entry, "alarm-type-qualifier")},
-                        InventoryData{
-                            .resources = resources,
-                            .severities = severities,
-                        });
-                }
+        ietfAlarmsModule, [&](auto, auto, auto, auto, auto, auto) {
+            {
+                std::unique_lock lck{m_mtx};
+                m_inventoryDirty = true;
             }
-
             m_session.sendNotification(m_session.getContext().newPath("/ietf-alarms:alarm-inventory-changed", std::nullopt), sysrepo::Wait::No);
             return sysrepo::ErrorCode::Ok;
         },
@@ -286,7 +265,6 @@ Daemon::Daemon()
  * */
 std::optional<std::string> Daemon::inventoryValidationError(const InstanceKey& key, const std::string& severity)
 {
-    std::unique_lock lck{m_mtx};
     auto it = m_inventory.find(key.type);
     if (it == m_inventory.end()) {
         return "No alarm inventory entry for " + key.type.xpathIndex();
@@ -313,15 +291,21 @@ sysrepo::ErrorCode Daemon::submitAlarm(sysrepo::Session rpcSession, const libyan
     const auto alarmRoot = m_session.getData(rootPath);
     assert(alarmRoot);
 
-    if (auto inventoryError = inventoryValidationError(alarmKey, severity)) {
-        rpcSession.setNetconfError({.type = "application",
-                                    .tag = "data-missing",
-                                    .appTag = std::nullopt,
-                                    .path = std::nullopt,
-                                    .message = (inventoryError.value() + " -- see RFC8632 (sec. 4.1).").c_str(),
-                                    .infoElements = {}});
-        m_log->warn(inventoryError.value());
-        return sysrepo::ErrorCode::OperationFailed;
+    {
+        std::unique_lock lck{m_mtx};
+        if (m_inventoryDirty) {
+            rebuildInventory(*alarmRoot);
+        }
+        if (auto inventoryError = inventoryValidationError(alarmKey, severity)) {
+            rpcSession.setNetconfError({.type = "application",
+                                        .tag = "data-missing",
+                                        .appTag = std::nullopt,
+                                        .path = std::nullopt,
+                                        .message = (inventoryError.value() + " -- see RFC8632 (sec. 4.1).").c_str(),
+                                        .infoElements = {}});
+            m_log->warn(inventoryError.value());
+            return sysrepo::ErrorCode::OperationFailed;
+        }
     }
 
     auto matchedShelf = shouldBeShelved(m_session, alarmKey);
@@ -527,6 +511,34 @@ void Daemon::reshelve()
         m_session.applyChanges();
 
         updateAlarmSummary(m_session);
+    }
+}
+
+void Daemon::rebuildInventory(const libyang::DataNode& dataWithInventory)
+{
+    const auto data = dataWithInventory.findPath(alarmInventoryPrefix);
+    m_inventory.clear();
+    if (data->child()) {
+        for (const auto& entry : data->child()->siblings()) {
+            decltype(InventoryData::resources) resources;
+            decltype(InventoryData::severities) severities;
+            for (const auto& child : entry.immediateChildren()) {
+                const auto shortName = child.schema().name();
+                if (shortName == "resource") {
+                    resources.emplace(child.asTerm().valueStr());
+                } else if (shortName == "severity-level") {
+                    severities.emplace(child.asTerm().valueStr());
+                }
+            }
+            m_inventory.emplace(
+                Type{
+                    .id = utils::childValue(entry, "alarm-type-id"),
+                    .qualifier = utils::childValue(entry, "alarm-type-qualifier")},
+                InventoryData{
+                    .resources = resources,
+                    .severities = severities,
+                });
+        }
     }
 }
 }

src/alarms/Daemon.h
@@ -34,6 +34,7 @@ private:
     NotifyStatusChanges m_notifyStatusChanges;
     std::optional<int32_t> m_notifySeverityThreshold;
     std::mutex m_mtx;
+    bool m_inventoryDirty;
     std::unordered_map<Type, InventoryData, boost::hash<Type>> m_inventory;
 
     sysrepo::ErrorCode submitAlarm(sysrepo::Session rpcSession, const libyang::DataNode& input);
@@ -41,6 +42,7 @@ private:
     libyang::DataNode createStatusChangeNotification(const libyang::DataNode& alarmNode);
     std::optional<std::string> inventoryValidationError(const InstanceKey& key, const std::string& severity);
     void reshelve();
+    void rebuildInventory(const libyang::DataNode& dataWithInventory);
 };
 
 }

commit 73d1bbdad93a9aa5daee02c584265ecbe1af7adc

do not invalidate the alarm inventory cache
The cache was being rebuilt over and over again, even when there were no
changes to the inventory since the last time. What a silly bug.

The synthetic benchmark is 25% faster compared to the previous version.
Compared to the very beginning, it's 66% faster (and we're measuring an
extra purge which was not included when this benchmarking started).

Fixes: 2d15c1c lazy population of alarm inventory cache
Change-Id: I0ba443eafdcb36e32794c014fef57e5f2795b6b8

src/alarms/Daemon.cpp
@@ -498,6 +498,7 @@ void Daemon::rebuildInventory(const libyang::DataNode& dataWithInventory)
                 });
         }
     }
+    m_inventoryDirty = false;
 }
 
 void Daemon::updateStatistics(libyang::DataNode& edit)